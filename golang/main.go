bfd41cc1928fbb13731e24ec558f13a7fb53bf89
xakristl8 <xakristl8@bk.ru>
2024-09-28 17:14:32 +0300

    изменён фаил main.go

--- golang/main.go
+++ golang/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var name string = "Михаил"
+	var surname string = "Ткачёв"
+
+	fmt.Println(name, surname)
+
+	length := 8
+	width := 9
+
+	square := length * width
+
+	fmt.Print(square)
+
 }
