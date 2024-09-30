9b6ec6ed6341c161e8412912f3691e79251dab80
xakristl8 <xakristl8@bk.ru>
2024-09-30 20:15:27 +0300

    лаба4: обновлён фаил main.go (ошибка x 3,4 dsdjlbn NaN)

--- golang/main.go
+++ golang/main.go
@@ -7,12 +7,14 @@ import (
 )
 
 func main() {
-	var y float64 = 0
+	var y float64
 
-	var a float64 = 0
-	var b float64 = 0
-	var x float64 = 0
+	var a float64 = 2.5
+	var b float64 = 4.6
+	var x [5]float64 = [5]float64{1.2, 1.36, 1.57, 1.93, 2.25}
 
-	y = laba4.Colcul(a, b, x)
-	fmt.Println(y)
+	for i := 0; i < len(x); i++ {
+		y = laba4.Colcul(a, b, x[i])
+		fmt.Println("при x =", x[i], " y =", y)
+	}
 }
