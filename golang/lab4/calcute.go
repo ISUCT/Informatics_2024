50fc9a6b66a6f55e13d45a3ef34d1946a8516d3b
Иван Яшков <ivanaskov@MacBook-Pro-Ivan.local>
2024-10-02 13:27:54 +0300

    lab4

---
+++ golang/lab4/calcute.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Вариант 3
+// Функция для вычисления значения y
+func calculateY(x, a, b float64) float64 {
+	numerator := 1 + math.Log2(x/a)
+	denominator := b - x/math.Exp(a)
+	return numerator / denominator
+}
+
+func main() {
+	// Параметры функции
+	a := 2.0
+	b := 0.95
+
+	// Задача A
+	fmt.Println("Задача A:")
+	xH := 1.25    // Начальное значение x
+	xK := 2.75    // Конечное значение x
+	deltaX := 0.3 // Шаг изменения x
+
+	for x := xH; x <= xK; x += deltaX {
+		y := calculateY(x, a, b)
+		fmt.Printf("x = %.2f, y = %.4f\n", x, y)
+	}
+
+	// Задача B
+	fmt.Println("Задача B:")
+	xValues := []float64{2.2, 3.78, 4.51, 6.58, 1.2}
+
+	for _, x := range xValues {
+		y := calculateY(x, a, b)
+		fmt.Printf("x = %.2f, y = %.4f\n", x, y)
+	}
+}
