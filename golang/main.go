cd904d826d8140c6b91e1cdbb420272ba3768500
Quv1Li <pashazzz3737@gmail.com>
2024-10-04 21:07:42 +0300

    lab 4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/labs"
+)
 
 func main() {
-	fmt.Println("Zakharov Pavel Dmitrievich")
+	labs.Y_A()
+	fmt.Println("-------------------")
+	labs.Y_B()
 }
