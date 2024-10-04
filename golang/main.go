9d884f2bf39050911f9320b1a023fb5bd6f78131
xakristl8 <xakristl8@bk.ru>
2024-10-04 21:46:13 +0300

    laba4: завершина

--- golang/main.go
+++ golang/main.go
@@ -7,14 +7,21 @@ import (
 )
 
 func main() {
-	var y float64
+	var result float64
 
 	var a float64 = 2.5
 	var b float64 = 4.6
 	var x [5]float64 = [5]float64{1.2, 1.36, 1.57, 1.93, 2.25}
 
+	fmt.Println("\nзадача А:")
+	for i := 1.15; i < 3.05; i += 0.38 {
+		result = laba4.Colcul(a, b, i)
+		fmt.Printf("при x = %.2f, y = %.2f \n", i, result)
+	}
+
+	fmt.Println("\nзадача B:")
 	for i := 0; i < len(x); i++ {
-		y = laba4.Colcul(a, b, x[i])
-		fmt.Println("при x =", x[i], " y =", y)
+		result = laba4.Colcul(a, b, x[i])
+		fmt.Println("при x =", x[i], " y =", result)
 	}
 }
