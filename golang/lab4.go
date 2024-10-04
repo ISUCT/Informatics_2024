85b97f834b5b2493d011b85e436a73cc37c2b068
Ruslan <ruslan9051076919@gmail.com>
2024-10-04 20:41:49 +0300

    lab4 Kulikov Ruslan

---
+++ golang/lab4.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// №10
+
+func f(x float64, a float64, b float64) float64 {
+	return math.Acos(math.Pow(x, 2)-math.Pow(b, 2)) / math.Asin(math.Pow(x, 2)-math.Pow(a, 2))
+}
+
+func main() {
+	//Задание А
+	fmt.Println("Задание А")
+	fmt.Println("x\ty")
+	a := 0.05
+	b := 0.06
+	xn := 0.2
+	xk := 0.95
+	deltaX := 0.15
+	for x := xn; x <= xk; x += deltaX {
+		fmt.Printf("%.2f\t%.2f\n", x, f(x, a, b))
+	}
+
+	//Задание B
+	fmt.Println("Задание B")
+	fmt.Println("x\ty")
+	iks := []float64{0.15, 0.26, 0.37, 0.48, 0.56}
+	for _, x := range iks {
+
+		fmt.Printf("%.2f\t%.2f\n", x, f(x, a, b))
+	}
+
+}
