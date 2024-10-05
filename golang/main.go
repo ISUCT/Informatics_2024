53d219eae86255a85faf0d5afcbd03c8601405cf
Ruslan <ruslan9051076919@gmail.com>
2024-10-05 21:06:06 +0300

    main

--- golang/main.go
+++ golang/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	lab4 "isuct.ru/informatics2022/labs"
 )
 
+// путь:"isuct.ru/informatics2022/labs"
 func main() {
 	fmt.Println("Куликов Руслан Игоревич")
+	lab4.RunLab4()
 }
