6da96a46f1bcffa0ce906a2d2bf9bf67195bc1c5
Nikitapon37 <pon37ponpon@gmail.com>
2024-10-05 10:54:02 +0300

    4lab var 4

---
+++ golang/lab4.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func colcul(x float64) float64 {
+	y := math.Pow(math.Abs(float64(x*x-2.5)), 0.25) + math.Pow(math.Log10(x*x), 0.33333333)
+	return y
+}
+
+func main() {
+	var result float64
+	var x [5]float64 = [5]float64{1.84, 2.71, 3.81, 4.56, 5.62}
+	fmt.Println("Задача A")
+	for i := 1.25; i <= 3.25; i += 0.40 {
+		result = colcul(i)
+		fmt.Println("x = ", i, "y = ", result)
+	}
+	fmt.Println("Задача B")
+	for i := 0; i < len(x); i++ {
+		result = colcul(x[i])
+		fmt.Println("При x = ", x[i], "y = ", result)
+	}
+}
