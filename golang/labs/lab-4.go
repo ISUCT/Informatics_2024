22fef566ff0034a6fcdaeeafa79f4cdf82dc12c9
qwwqqqw <pshenichnikov6002@gmail.com>
2024-10-05 12:08:38 +0300

    Create folder for laboratory

---
+++ golang/labs/lab-4.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func logarifm(x, base float64) float64 {
+	return math.Log(x) / math.Log(base)
+}
+
+func Calc(a, b, x float64) float64 {
+	koren := math.Cbrt(x)
+	log5 := logarifm(x, 5)
+	lgkub := math.Pow(math.Log(x-1), 3)
+	y := (a*koren - b*log5) / lgkub
+	return y
+}
+
+func main() {
+	var a float64 = 4.1
+	var b float64 = 2.7
+
+	// Задача A
+	for x := 1.5; x <= 3.5; x += 0.4 {
+		y := Calc(a, b, x)
+		fmt.Println("Задача A:")
+		fmt.Printf("x = , y = \n", x, y)
+	}
+
+	// Задача B
+	xZnach := []float64{1.9, 2.15, 2.34, 2.74, 3.16}
+	for _, x := range xZnach {
+		y := Calc(a, b, x)
+		fmt.Println("\nЗадача B:")
+		fmt.Printf("x = , y = \n", x, y)
+	}
+}
