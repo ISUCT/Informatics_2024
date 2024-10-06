46e2c70a78fea63497af81499a80587c9393d1ac
qwwqqqw <pshenichnikov6002@gmail.com>
2024-10-06 15:10:30 +0300

    пайплайн

--- golang/labs/lab-4.go
+++ golang/labs/lab-4.go
@@ -21,18 +21,16 @@ func main() {
 	var a float64 = 4.1
 	var b float64 = 2.7
 
-	// Задача A
+	fmt.Println("Задача A:")
 	for x := 1.5; x <= 3.5; x += 0.4 {
 		y := Calc(a, b, x)
-		fmt.Println("Задача A:")
-		fmt.Printf("x = , y = \n", x, y)
+		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
 	}
 
-	// Задача B
+	fmt.Println("\nЗадача B:")
 	xZnach := []float64{1.9, 2.15, 2.34, 2.74, 3.16}
 	for _, x := range xZnach {
 		y := Calc(a, b, x)
-		fmt.Println("\nЗадача B:")
-		fmt.Printf("x = , y = \n", x, y)
+		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
 	}
 }
