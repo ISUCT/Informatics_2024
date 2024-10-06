13a623994e5a431d130875bd584a0e5c6b093907
xakristl8 <xakristl8@bk.ru>
2024-10-06 18:04:32 +0300

    испровления

--- golang/laba4/laba4_test.go
+++ golang/laba4/laba4_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestColcul(t *testing.T) {
-	result := laba4.Colcul(2.5, 4.6, 1.2)
+	var result float64 = laba4.Colcul(2.5, 4.6, 1.2)
 	if result != 0.588691 {
 		t.Fatalf("Colcul(2.5, 4.6, 1.2) = %f, want 0.588691, error", result)
 	}
