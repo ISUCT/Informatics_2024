61a204b5f7bc46bc555632d772f0bd5dd9dbc3d1
alisa_chin <chindilovaei@gmail.com>
2024-10-07 19:26:11 +0300

    Chindilova

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -6,6 +6,7 @@ import (
 
 func Function(x float64) float64 {
 	var b float64 = 2.5
+
 	y := (1 + math.Pow(math.Sin(math.Pow(b, 3)+math.Pow(x, 3)), 2)) / (math.Cbrt((math.Pow(b, 3) + math.Pow(x, 3))))
 	return y
 }
