5b2b535f8c6d20a3ab74263b7243ed4f684c86be
Vladislav3707 <mr8815542@gmail.com>
2024-10-07 15:19:40 +0300

    added initial lab4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import "isuct.ru/informatics2022/lab4"
+
+const a float64 = 2.0
+const b float64 = 0.95
 
 func main() {
-	fmt.Println("Hello world")
+	lab4.TaskA(b, a, 1.25, 2.75, 0.3)
+	var p = [5]float64{2.2, 3.78, 4.51, 6.58, 1.2}
+	lab4.TaskB(a, b, p)
 }
