bcaa3ed0e91b54d3f80b393fe43040ce4722c774
ISUCT <ads@mail.ru>
2024-10-07 14:05:05 +0300

    Fix main.go

---
+++ golang/lab/lab4.go
@@ -0,0 +1,22 @@
+package lab
+
+import (
+	"fmt"
+	"math"
+)
+
+func Calc(b float64, x float64) float64 {
+	return (1 + math.Pow(math.Sin(math.Pow(b, 3)+math.Pow(x, 3)), 2)) / (math.Cbrt(math.Pow(b, 3) + math.Pow(x, 3)))
+}
+
+func TaskA(b float64, Xn float64, Xk float64, delX float64) {
+	for x := Xn; x <= Xk; x += delX {
+		fmt.Println("При x =", x, "y =", Calc(b, x))
+	}
+}
+
+func TaskB(b float64, x [5]float64) {
+	for _, value := range x {
+		fmt.Println("При x =", value, "y =", Calc(b, value))
+	}
+}
--- golang/main.go
+++ golang/main.go
@@ -7,7 +7,7 @@ import (
 const b float64 = 2.5
 
 func main() {
-	TaskA(b, 1.28, 3.28, 0.4)
+	lab.TaskA(b, 1.28, 3.28, 0.4)
 	var slice = [5]float64{1.1, 2.4, 3.6, 1.7, 3.9}
-	TaskB(b, slice)
+	lab.TaskB(b, slice)
 }
