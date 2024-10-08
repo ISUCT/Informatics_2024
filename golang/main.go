2d9d6d63ce387ac017abee7c9961b2c1b2246b41
Kirill <svakewow@gmail.com>
2024-10-08 20:32:07 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/lab4"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var x float64 = 2.4 // x1=2.4  x2=2.8 x3=3.9 x4=4.7 x5=3.16
+	fmt.Println(lab4.Mathfunc(7.2, 1.3, x))
 }
