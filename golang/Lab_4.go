53e32ceac38124d488766fcd3ffe32b6f0874bc8
Amoral <slavaogudin@gmail.com>
2024-10-08 15:51:55 +0300

    Added initial lab

--- golang/Lab_4.go
+++ golang/Lab_4.go
@@ -1,29 +1,22 @@
-package main
+package Lab_4
 
 import (
 	"fmt"
 	"math"
 )
 
-func Calculator(a, b, x float64) float64 {
-	return (1 + math.Pow(math.Log10(x/a), 2)) / (b - math.Pow(2.7, x/a))
+func Calc(b float64, x float64) float64 {
+	return (1 + math.Pow(math.Sin(math.Pow(b, 3)+math.Pow(x, 3)), 2)) / (math.Cbrt(math.Pow(b, 3) + math.Pow(x, 3)))
 }
 
-func TaskA(a, b, Xn, Xk, delX float64) {
+func TaskA(b float64, Xn float64, Xk float64, delX float64) {
 	for x := Xn; x <= Xk; x += delX {
-		fmt.Printf("x: %.2f, Result: %.4f\n", x, Calculator(a, b, x))
+		fmt.Println("При x =", x, "y =", Calc(b, x))
 	}
 }
 
-func TaskB(a, b float64, arr []float64) {
-	for _, value := range arr {
-		fmt.Printf("x: %.2f, Result: %.4f\n", value, Calculator(a, b, value))
+func TaskB(b float64, x [5]float64) {
+	for _, value := range x {
+		fmt.Println("При x =", value, "y =", Calc(b, value))
 	}
-}
-
-func main() {
-	TaskA(2.0, 5.0, 1.0, 10.0, 1.0)
-
-	values := []float64{1.0, 2.0, 3.0, 4.0, 5.0}
-	TaskB(2.0, 5.0, values)
 }
