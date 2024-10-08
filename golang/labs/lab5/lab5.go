a65f49caf677018ef2ef03a43184e7175d8fd5d6
kinzool <151194968+kinzool@users.noreply.github.com>
2024-10-08 08:50:15 +0300

    Update lab5.go

--- golang/labs/lab5/lab5.go
+++ golang/labs/lab5/lab5.go
@@ -43,7 +43,7 @@ func (c Computer) GetRamBrand() string {
 }
 
 func WorkWithComputer() {
- myPc := lab5.Computer{Hdd: 512, HddBrand: "Asus", Ram: 16, RamBrand: "Corsair"}
+ myPc := Computer{Hdd: 512, HddBrand: "Asus", Ram: 16, RamBrand: "Corsair"}
 	fmt.Printf("Объем оперативной памяти: %d Gb\n", myPc.GetRam())
 	myPc.SetRam(32)
 	fmt.Printf("Объем оперативной памяти: %d Gb\n", myPc.GetRam())
@@ -56,7 +56,7 @@ func WorkWithComputer() {
 
 	fmt.Println()
 
-	newPc := lab5.NewComputer(2048, "WD Blue", 64, "HyperX")
+	newPc := NewComputer(2048, "WD Blue", 64, "HyperX")
 	fmt.Printf("Объем жесткого диска нового компьютера: %d Gb\n", newPc.GetHdd())
 	newPc.SetHdd(1024)
 	fmt.Printf("Объем жесткого диска нового компьютера после изменений: %d Gb\n", newPc.GetHdd())
