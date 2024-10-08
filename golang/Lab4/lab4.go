0286422d0247968c93bcf812df866d5cc7f36902
Skane122 <HARDYWITHUS@yandex.ru>
2024-10-08 21:04:33 +0300

    lab4

--- golang/Lab4/lab4.go
+++ golang/Lab4/lab4.go
@@ -1,4 +1,4 @@
-package main
+package Lab4
 
 import (
 	"fmt"
@@ -12,11 +12,21 @@ func calculate(x float64) float64 {
 	return math.Pow(math.Pow(math.Asin(x), 4)+math.Pow(math.Acos(x), 6), n)
 }
 
-func main() {
+func Task_A() {
 
 	for x := 0.22; x <= 0.92; x += 0.14 {
 		result := calculate(x)
 		fmt.Printf("результат от %.2f : %.2f\n", x, result)
+
+	}
+
+}
+
+func Task_B() {
+	var arr = [5]float64{0.1, 0.35, 0.4, 0.55, 0.6}
+	for _, x := range arr {
+		answer := calculate(x)
+		fmt.Println(answer)
 
 	}
 }
--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/Lab4"
+)
 
 func main() {
 	fmt.Println("Semenenko Mikhail")
+	Lab4.Task_A()
+	Lab4.Task_B()
+
 }
