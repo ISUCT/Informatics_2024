b71f119e8d170100423d47140890a3850e5bb283
nerocos0 <rybka767676@gmail.com>
2024-10-10 00:21:53 +0300

    make system package-main

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,4 +1,4 @@
-package main
+package lab4
 
 import (
 	"fmt"
@@ -6,18 +6,11 @@ import (
 	"strconv"
 )
 
-func main() {
-	const const_a, const_b = 2, 3
-	task_A(const_a, const_b)
-	fmt.Println()
-	task_B(const_a, const_b)
-}
-
 func return_value_y(x1 float64, const_a float64, const_b float64) float64 {
 	y := ((math.Asin(math.Pow(x1, const_a))) + (math.Acos(math.Pow(x1, const_b))))
 	return y
 }
-func task_A(const_a float64, const_b float64) {
+func Task_A(const_a float64, const_b float64) {
 	all_x := []float32{}
 	var x_n float32 = 0.11
 	var x_k float32 = 0.36
@@ -29,7 +22,7 @@ func task_A(const_a float64, const_b float64) {
 		fmt.Println(strconv.Itoa(i+1)+":", return_value_y(float64(x), const_a, const_b))
 	}
 }
-func task_B(const_a float64, const_b float64) {
+func Task_B(const_a float64, const_b float64) {
 	five_x := [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}
 	for i, x := range five_x {
 		fmt.Println(strconv.Itoa(i+1)+":", return_value_y(x, const_a, const_b))
--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/lab4"
+)
 
 func main() {
+	const const_a = 2
+	const const_b = 3
 	fmt.Println("Лутохин Никита Евгеньевич")
+	lab4.Task_A(const_a, const_b)
+	println("")
+	lab4.Task_B(const_a, const_b)
 }
