7a2f88ced9afde40ab3ad9a17303b8a22bdc69a8
Viktoria3284 <vikamedvedeva879@gmail.com>
2024-10-11 20:25:46 +0300

    сделала 3 лабораторную

--- golang/main.go
+++ golang/main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	func_ "isuct.ru/informatics2022/code"
-)
+import "fmt"
 
 func main() {
-	var a float64 = 2.0
-	var xs = [8]float64{1.2, 4.2, 0.6, 1.16, 1.32, 1.47, 1.65, 1.93}
-	for _, x := range xs {
-		fmt.Println("y =", func_.Lab_4(a, x))
-	}
+	fmt.Println("Медведева Виктория Александровна")
 }
