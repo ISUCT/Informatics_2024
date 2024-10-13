8a7505028c619618be7cf0d8e24f2e5d70452106
rukinapolina <polinostka@gmail.com>
2024-10-13 22:47:51 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"isuct.ru/informatics2022/lab4"
+    "fmt"
+    "isuct.ru/informatics2022/lab4"
 )
 
 const a float64 = 2.0
 
 func main() {
-	lab4.TaskA(a, 1.08, 1.88, 0.16)
-	var slice = [5]float64{1.16, 1.35, 1.48, 1.52, 1.96}
-	lab4.TaskB(a, slice)
+    fmt.Println("Рукина Полина Владимировна")
+
+    valuesA := lab4.TaskA(a, 1.08, 1.88, 0.16)
+    var slice = [5]float64{1.16, 1.35, 1.48, 1.52, 1.96}
+    valuesB := lab4.TaskB(a, slice)
+
+    for _, value := range valuesA {
+        fmt.Println(value)
+    }
+    
+    for _, value := range valuesB {
+        fmt.Println(value)
+    }
 }
