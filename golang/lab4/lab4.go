f3c66edba7635e3ac8fc690f15d5a1ab2525aea4
rukinapolina <polinostka@gmail.com>
2024-10-13 22:47:16 +0300

    Update lab4.go
    Функция под номером 29

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,20 +1,25 @@
 package lab4
 
 import (
-	 "fmt"
-	 "math"
+    "math"
 )
 
-func Calc(a float64,x float64) float64 {
-	return math.Tan (math.Pow(math.Log10(a+x),3))/math.Pow(a+x,2/7)
+func Calc(a float64, x float64) float64 {
+    return math.Tan(math.Pow(math.Log10(a+x), 3)) / math.Pow(a+x, 2.0/7.0)
 }
-func TaskA( a float64,xi float64, xk float64,deltaX float64) {
-	for x:=xi;x<=xk;x +=deltaX{
-		fmt.Println (Calc(a,x))
-	}
+
+func TaskA(a float64, xi float64, xk float64, deltaX float64) []float64 {
+    values := []float64{}
+    for x := xi; x <= xk; x += deltaX {
+        values = append(values, Calc(a, x))
+    }
+    return values
 }
-func TaskB(a float64, x [5] float64){
-	for _, value := range x{
-		fmt.Println (Calc(a,value))
-	}
+
+func TaskB(a float64, x [5]float64) []float64 {
+    values := []float64{}
+    for _, value := range x {
+        values = append(values, Calc(a, value))
+    }
+    return values
 }
