2a3b8ce7b11d8504d6364bc634a8c1115fb96288
Quv1Li <pashazzz3737@gmail.com>
2024-10-13 21:47:13 +0300

    update lab4

--- golang/labs/lab4/lab4.go
+++ golang/labs/lab4/lab4.go
@@ -1,22 +1,26 @@
 package labs
 
 import (
+	"fmt"
 	"math"
 )
 
 // Лабораторная 4, задание по функции 6
-func y(x float64) float64 {
-	return math.Pow(1.2, x) - math.Pow(x, 1.2)
+func calculate_y(x float64) float64 {
+	if math.Abs(x) >= 1 {
+		return math.Pow(1.2, x) - math.Pow(x, 1.2)
+	} else {
+		return math.Acos(x)
+	}
 }
 
 // Задача под А
 func Task_A(begin_x, end_x, delta_x float64) []float64 {
 	answer_arr := make([]float64, 0)
 
-	for begin_x < end_x {
-		x := begin_x
-		answer_arr = append(answer_arr, y(x))
-		begin_x += delta_x
+	for i := begin_x; i < end_x; i += delta_x {
+		x := i
+		answer_arr = append(answer_arr, calculate_y(x))
 	}
 
 	return answer_arr
@@ -27,8 +31,17 @@ func Task_B(arguments []float64) []float64 {
 	answer_arr := make([]float64, 0)
 
 	for _, x := range arguments {
-		answer_arr = append(answer_arr, y(x))
+		answer_arr = append(answer_arr, calculate_y(x))
 	}
 
 	return answer_arr
+}
+
+func RunLab4Task() {
+	fmt.Println("------------------------------------------")
+	fmt.Println(Task_A(0.2, 2.2, 0.4))
+	fmt.Println("------------------------------------------")
+	arr := [5]float64{0.1, 0.9, 1.2, 1.5, 2.3}
+	fmt.Println(Task_B(arr[:]))
+	fmt.Println("------------------------------------------")
 }
--- golang/main.go
+++ golang/main.go
@@ -7,11 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Zakharov Pavel Dmitrievich")
 	// Задание 4
-	fmt.Println("------------------------------------------")
-	fmt.Println(labs.Task_A(0.2, 2.2, 0.4))
-	fmt.Println("------------------------------------------")
-	arr := [5]float64{0.1, 0.9, 1.2, 1.5, 2.3}
-	fmt.Println(labs.Task_B(arr[:]))
-	fmt.Println("------------------------------------------")
+	labs.RunLab4Task()
 }
