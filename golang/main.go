f8d6fcee13738a5ed0dd8698ef351e433037ee3a
Kirill <svakewow@gmail.com>
2024-10-14 20:25:07 +0300

    fix taskA

--- golang/main.go
+++ golang/main.go
@@ -10,14 +10,14 @@ func main() {
 	var res float64
 	var elements = [5]float64{2.4, 2.8, 3.9, 4.7, 3.16}
 	fmt.Println("Задача B \n")
-	for i := 0; i < len(elements); i++ {
-		res = lab4.Mathfunc(7.2, 1.3, elements[i])
-		fmt.Println("При x =", elements[i], "функция равна: \n", res)
+	for _, i := range elements {
+		res = lab4.Mathfunc(7.2, 1.3, i)
+		fmt.Println("При x =", i, "функция равна: \n", res)
 	}
 	fmt.Println("Задача А \n")
 	var xn, xk, xdel float64 = 1.56, 4.71, 0.63
 	for i := xn; i <= xk; i += xdel {
-		fmt.Println("При x =", i, "функция равна: \n", res)
+		fmt.Println("При x =", i, "функция равна: \n", lab4.Mathfunc(7.2, 1.3, i))
 	}
 	fmt.Println("Нерабеев Кирилл Сергеевич")
 }
