58ba21cb4228e58a1952574896758f8c0d5db7a6
Kirill <svakewow@gmail.com>
2024-10-14 19:57:27 +0300

    lab4 fix

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -4,6 +4,7 @@ import (
 	"math"
 )
 
+// func
 func Mathfunc(a, b, elements float64) float64 {
 	y := math.Pow((a+b*elements)/(math.Pow(math.Log10(elements), 3)), 0.2)
 	return y
--- golang/main.go
+++ golang/main.go
@@ -9,14 +9,14 @@ import (
 func main() {
 	var res float64
 	var elements = [5]float64{2.4, 2.8, 3.9, 4.7, 3.16}
-	fmt.Println("Задача A \n")
+	fmt.Println("Задача B \n")
 	for i := 0; i < len(elements); i++ {
 		res = lab4.Mathfunc(7.2, 1.3, elements[i])
 		fmt.Println("При x =", elements[i], "функция равна: \n", res)
 	}
-	fmt.Println("Задача B \n")
-	for i := 1.56; i <= 4.71; i += 0.63 {
-		res = lab4.Mathfunc(7.2, 1.3, i)
+	fmt.Println("Задача А \n")
+	var xn, xk, xdel float64 = 1.56, 4.71, 0.63
+	for i := xn; i <= xk; i += xdel {
 		fmt.Println("При x =", i, "функция равна: \n", res)
 	}
 	fmt.Println("Нерабеев Кирилл Сергеевич")
