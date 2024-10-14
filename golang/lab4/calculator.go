fce38723620a3f9e92059a2d39d6a06e80e3a199
Habicht330 <nikitakorolev27@gmail.com>
2024-10-14 22:19:06 +0300

    lab4

--- golang/lab4/calculator.go
+++ golang/lab4/calculator.go
@@ -2,22 +2,25 @@ package lab4
 
 import (
 	"math"
-	"fmt"
 )
 
 func Calculator(x, a float64) float64 {
-	y := math.Pow(a, math.Sqrt(x)-1) - math.Log10(math.Sqrt(x) - 1) + math.Pow(math.Sqrt(x)-1, 1.0/3.0)
+	y := math.Pow(a, math.Sqrt(x)-1) - math.Log10(math.Sqrt(x)-1) + math.Pow(math.Sqrt(x)-1, 1.0/3.0)
 	return y
 }
 
-func TaskA(b, Xn,  Xk, delX float64) {
+func TaskA(b, Xn, Xk, delX float64) []float64 {
+	var y []float64
 	for x := Xn; x <= Xk; x += delX {
-		fmt.Println(Calculator(b, x))
+		y = append(y, Calculator(b, x))
 	}
+	return y
 }
 
-func TaskB(b float64, x [5]float64) {
+func TaskB(b float64, x [5]float64) []float64 {
+	var y []float64
 	for _, value := range x {
-		fmt.Println(Calculator(b, value))
+		y = append(y, Calculator(b, value))
 	}
+	return y
 }
--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"isuct.ru/informatics2022/lab4"
 )
 
-const a float64 = 2.5
+const b float64 = 2.5
 
 func main() {
-	lab4.TaskA(a, 1.2, 3.7, 0.5)
+	fmt.Println(lab4.TaskA(b, 1.2, 3.7, 0.5))
 	var s = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
-	lab4.TaskB(a, s)
+	fmt.Println(lab4.TaskB(b, s))
 }
