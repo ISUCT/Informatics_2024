1e547618b98b5f6cf3f5084c8b64b2de6577703a
xakristl8 <xakristl8@bk.ru>
2024-10-15 06:52:56 +0300

    исправлено тестирование 4 лабы

--- golang/main.go
+++ golang/main.go
@@ -15,11 +15,9 @@ func main() {
 	var xMax float64 = 3.05
 	var xDelta float64 = 0.38
 	var x []float64 = []float64{1.2, 1.36, 1.57, 1.93, 2.25}
-	var resultA []float64
-	var resultB []float64
 
-	resultA = laba4.TaskA(a, b, xMin, xMax, xDelta)
+	var resultA []float64 = laba4.TaskA(a, b, xMin, xMax, xDelta)
 	fmt.Println(resultA)
-	resultB = laba4.TaskB(a, b, x)
+	var resultB []float64 = laba4.TaskB(a, b, x)
 	fmt.Println(resultB)
 }
