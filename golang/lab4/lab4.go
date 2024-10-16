4f11d8099c0c3bb3a6406fa65593e771e65820a7
TheScarletLight <lemoncheit@gmail.com>
2024-10-16 13:42:39 +0300

    lab4

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,20 +1,35 @@
-package lab4
+package lab4 
+ 
+import ( 
+	"math"
+	"fmt"
+) 
+ 
+func  CalculateExpression (a float64, x float64) float64 { 
+    return math.Tan(math.Pow(math.Log10(a+x), 3)) / math.Pow(a+x, 2.0/7.0) 
+} 
+ 
+func TaskA(a , xi , xk , deltax float64) []float64 { 
+     var values []float64 
+    for x := xi; x <= xk; x += deltax { 
+        values = append(values,  CalculateExpression (a, x)) 
+    } 
+    return values 
+} 
+ 
+func TaskB(a float64, x [5]float64) []float64 { 
+    var values []float64
+    for _, value := range x {
+        values = append(values,  CalculateExpression (a, value)) 
+    } 
+    return values
+} 
 
-import (
-	 "fmt"
-	 "math"
-)
 
-func Calc(a float64,x float64) float64 {
-	return math.Tan (math.Pow(math.Log10(a+x),3))/math.Pow(a+x,2/7)
-}
-func TaskA( a float64,xi float64, xk float64,deltaX float64) {
-	for x:=xi;x<=xk;x +=deltaX{
-		fmt.Println (Calc(a,x))
-	}
-}
-func TaskB(a float64, x [5] float64){
-	for _, value := range x{
-		fmt.Println (Calc(a,value))
-	}
+func RunLab4Tasks() {
+    const a float64 = 2.0
+
+    fmt.Println(TaskA(a, 1.08, 1.88, 0.16))
+	var s = [5]float64{1.16, 1.35, 1.48, 1.52, 1.96}
+	fmt.Println(TaskB(a,s))
 }
--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,13 @@
-package main
-
-import (
-	"isuct.ru/informatics2022/lab4"
-)
-
-const a float64 = 2.0
-
-func main() {
-	lab4.TaskA(a, 1.08, 1.88, 0.16)
-	var slice = [5]float64{1.16, 1.35, 1.48, 1.52, 1.96}
-	lab4.TaskB(a, slice)
+package main 
+ 
+import ( 
+    "fmt" 
+    "isuct.ru/informatics2022/lab4" 
+) 
+ 
+const a float64 = 2.0 
+ 
+func main() { 
+    fmt.Println("Рукина Полина Владимировна") 
+    lab4.RunLab4Tasks()
 }
