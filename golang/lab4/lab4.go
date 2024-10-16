e52d8ab1983637064b15393eab1af7ef303395f5
GypsyGang37 <bobartem04@gmail.com>
2024-10-16 13:50:02 +0300

    я сделал

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -2,10 +2,20 @@ package lab4
 
 import (
 	"math"
+	"fmt"
 )
 
-func Calculator(a, b, x float64) float64 {
+func Calculate(a, b, x float64) float64 {
 	y := math.Cbrt(a*x+b) / (math.Log(x) * math.Log(x))
 
 	return y
+}
+
+
+func Enter(a, b float64, arr []float64){
+	for index, value := range arr {
+		// fmt.Printf(index)
+		y := Calculate(a, b, value)
+		fmt.Printf("X%d = %g\n", index, y)
+	}
 }
--- golang/main.go
+++ golang/main.go
@@ -10,18 +10,12 @@ const a float64 = 1.35
 const b float64 = 0.98
 
 func main() {
-	var A [6]float64 = [6]float64{1.14, 1.76, 2.38, 3, 3.62, 4.24}
-	var B [5]float64 = [5]float64{0.35, 1.28, 3.51, 5.21, 4.16}
+	var A []float64 = []float64{1.14, 1.76, 2.38, 3, 3.62, 4.24}
+	var B []float64 = []float64{0.35, 1.28, 3.51, 5.21, 4.16}
 
 	fmt.Println("Задача A:")
-	for i := 0; i < 6; i++ {
-		y := lab4.Calculator(a, b, A[i])
-		fmt.Printf("X%d = %g\n", i, y)
-	}
+	lab4.Enter(a, b, A)
 
 	fmt.Println("Задача B:")
-	for i := 0; i < 5; i++ {
-		y := lab4.Calculator(a, b, B[i])
-		fmt.Printf("X%d = %g\n", i, y)
-	}
+	lab4.Enter(a, b, B)
 }
