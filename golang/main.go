d85335662c8e027d7e16de4a308edeb0c1d5dbb4
Alexander56330 <gog-aleksander.gogolev@yandex.ru>
2024-10-16 13:37:22 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -9,21 +9,19 @@ import (
 
 
 func main() {
-	//Параметры функции.
 	a := -2.5
 
 	//Задача A
 	fmt.Println("Задача A:")
-xН := 3.5  // начальное знакчение x
-xК := 6.5  // конечное значение x
-deltax := 0.6  // шаг изменения x
+xН := 3.5
+xК := 6.5
+deltax := 0.6
 
 for x := xН; x <= xК; x += deltax {
 	y := lab4.CalculateY(x, a)
 	fmt.Printf("x = %.2f, y = %.4f\n", x, y)
 }
 
-//Задача В
 fmt.Println("Задача B:")
 xValues := []float64{2.89, 3.54, 5.21, 6.28, 3.48}
 
