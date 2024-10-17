7224137c0739de1cf895c7cd46a448682fff63e0
log69555 <143123261+log69555@users.noreply.github.com>
2024-10-17 17:17:21 +0300

    сделано

--- golang/main.go
+++ golang/main.go
@@ -10,7 +10,7 @@ const a float64 = 1.35
 const b float64 = 0.98
 
 func main() {
-	var A []float64 = []float64{1.14, 1.76, 2.38, 3, 3.62, 4.24}
+	A := lab4.TaskA(1.14, 4.24, 0.62)
 	var B []float64 = []float64{0.35, 1.28, 3.51, 5.21, 4.16}
 
 	fmt.Println("Задача A:")
