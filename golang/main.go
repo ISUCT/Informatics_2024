b8ed59cbae54fecd4271a0db477826f89fa69763
nerocos0 <rybka767676@gmail.com>
2024-10-18 23:17:48 +0300

    made lab6, modified lab4(for working need call only 1 func)

--- golang/main.go
+++ golang/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/labs"
 )
 
 func main() {
-	const const_a = 2
-	const const_b = 3
 	fmt.Println("Лутохин Никита Евгеньевич")
-	lab4.Task_A(const_a, const_b)
-	println("")
-	lab4.Task_B(const_a, const_b)
+	labs.Go_lab4()
+	labs.Go_lab6()
 }
