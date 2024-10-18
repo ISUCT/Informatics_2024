893395924caa0c1c6a7fbc172b8f0e391b023269
alisa_chin <chindilovaei@gmail.com>
2024-10-18 22:13:48 +0300

    Chindilova_lab4_test

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "isuct.ru/informatics2022/lab4"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/lab4"
+)
 
 func main() {
+	fmt.Println("Чиндилова Алиса Денисовна")
 	lab4.RunLab4()
 }
