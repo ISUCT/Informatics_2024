864c2b76acfee649c013152058c56a1eed170501
qwwqqqw <pshenichnikov6002@gmail.com>
2024-10-18 21:10:11 +0300

    Update lab-4.go

--- golang/labs/lab-4.go
+++ golang/labs/lab-4.go
@@ -1,4 +1,4 @@
-package main
+package lab4
 
 import (
 	"fmt"
@@ -9,28 +9,35 @@ func logarifm(x, base float64) float64 {
 	return math.Log(x) / math.Log(base)
 }
 
-func Calc(a, b, x float64) float64 {
+func calculateY(a, b, x float64) float64 {
 	koren := math.Cbrt(x)
 	log5 := logarifm(x, 5)
 	lgkub := math.Pow(math.Log(x-1), 3)
-	y := (a*koren - b*log5) / lgkub
-	return y
+
+	return (a*koren - b*log5) / lgkub
 }
 
-func main() {
+func taskA(a, b float64) {
+	fmt.Println("Задача A:")
+	for x := 1.5; x <= 3.5; x += 0.4 {
+		y := calculateY(a, b, x)
+		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
+	}
+}
+
+func taskB(a, b float64) {
+	fmt.Println("\nЗадача B:")
+	xValues := []float64{1.9, 2.15, 2.34, 2.74, 3.16}
+	for _, x := range xValues {
+		y := calculateY(a, b, x)
+		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
+	}
+}
+
+func runlab4() {
 	var a float64 = 4.1
 	var b float64 = 2.7
 
-	fmt.Println("Задача A:")
-	for x := 1.5; x <= 3.5; x += 0.4 {
-		y := Calc(a, b, x)
-		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
-	}
-
-	fmt.Println("\nЗадача B:")
-	xZnach := []float64{1.9, 2.15, 2.34, 2.74, 3.16}
-	for _, x := range xZnach {
-		y := Calc(a, b, x)
-		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
-	}
+	taskA(a, b)
+	taskB(a, b)
 }
