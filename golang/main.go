9c2f39a374ae7804be1f49eb111c1145e520cf68
uhizaki <stas666xz@gmail.com>
2024-10-18 19:28:19 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/lab4"
+)
 
 func main() {
-	fmt.Println("Игнатенко Станислав Алексеевич")
+	var result float64
+	var a float64 = 1.1
+	var b float64 = 0.09
+	var x [5]float64 = [5]float64{1.21, 1.76, 2.53, 3.48, 4.52}
+	fmt.Println("\nзадача A:")
+	for i := 1.2; i < 2.2; i += 0.2 {
+		result = lab4.Colcul(a, b, i)
+		fmt.Println("при x=", i, "y=", result)
+	}
+	fmt.Println("\nзадача B:")
+	for _, i := range x {
+		result = lab4.Colcul(a, b, i)
+		fmt.Println("при x=", i, "y=", result)
+	}
 }
