488cce6ce2ea7374ba8eb48d7cbd9c5d6360eb84
nerocos0 <rybka767676@gmail.com>
2024-10-19 21:44:43 +0300

    fixed mistakes

--- golang/labs/lab4.go
+++ golang/labs/lab4.go
@@ -6,14 +6,12 @@ import (
 )
 
 func Go_lab4() {
-	fmt.Println()
 	const const_a = 2
 	const const_b = 3
-	for _, value := range Task_A(const_a, const_b) {
+	for _, value := range Task_A(const_a, const_b, 0.11, 0.36, 0.05) {
 		fmt.Println(value)
 	}
-	println("")
-	for _, value := range Task_B(const_a, const_b) {
+	for _, value := range Task_B(const_a, const_b, [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}) {
 		fmt.Println(value)
 	}
 }
@@ -21,23 +19,14 @@ func Return_value_y(x1 float64, const_a float64, const_b float64) float64 {
 	y := ((math.Asin(math.Pow(x1, const_a))) + (math.Acos(math.Pow(x1, const_b))))
 	return y
 }
-func Task_A(const_a float64, const_b float64) []float64 {
-	all_x := []float32{}
-	var x_n float32 = 0.11
-	var x_k float32 = 0.36
-	var x_step float32 = 0.05
-	for x := x_n; x <= x_k; x += x_step {
-		all_x = append(all_x, x)
-	}
-
+func Task_A(const_a float64, const_b float64, x_n, x_k, x_step float32) []float64 {
 	y_list_taskA := []float64{}
-	for _, x := range all_x {
+	for x := x_n; x <= x_k; x += x_step {
 		y_list_taskA = append(y_list_taskA, Return_value_y(float64(x), const_a, const_b))
 	}
 	return y_list_taskA
 }
-func Task_B(const_a float64, const_b float64) []float64 {
-	five_x := [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}
+func Task_B(const_a float64, const_b float64, five_x [5]float64) []float64 {
 	y_list_taskB := []float64{}
 	for _, x := range five_x {
 		y_list_taskB = append(y_list_taskB, Return_value_y(x, const_a, const_b))
--- golang/tests/test_for_lab4.go
+++ golang/tests/test_for_lab4.go
@@ -28,7 +28,7 @@ func testing_taskA(t *testing.T) {
 		1.620871010842551,
 		1.6372494546297607,
 		1.6540889552598357}
-	y_list_output := labs.Task_A(2, 3)
+	y_list_output := labs.Task_A(2, 3, 0.11, 0.36, 0.05)
 	if len(y_list_output) != 6 {
 		t.Errorf("Returns %v y, expected %v y", len(y_list_output), len(y_list_true))
 	}
@@ -44,7 +44,7 @@ func testing_taskB(t *testing.T) {
 		1.650716645162541,
 		1.6707225698630865,
 		1.7060955449869923}
-	y_list_output := labs.Task_B(2, 3)
+	y_list_output := labs.Task_B(2, 3, [5]float64{0.08, 0.26, 0.35, 0.41, 0.53})
 	for i, y := range y_list_output {
 		if y != y_list_true[i] {
 			t.Errorf("The function was calculated incorrectly")
