25a2921e1a0dae6cfc11bbf29e1d905aedf750fc
unknown <alex592813@gmail.com>
2024-10-19 12:05:48 +0300

    4 laba Semenov Aleksey Dmitrievich

--- golang/4lab/4lab.go
+++ golang/4lab/4lab.go
@@ -1,6 +1,7 @@
 package fourlaba
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -14,4 +15,38 @@ func CalculateY(a, b, x float64) float64 {
 	y := logTerm / denominator
 
 	return y
+}
+
+func SolveTaskA(a, b, xStart, xEnd, step float64) []float64 {
+	results := []float64{}
+	for x := xStart; x <= xEnd; x += step {
+		y := CalculateY(a, b, x)
+		results = append(results, y)
+	}
+	return results
+}
+
+func SolveTaskB(a, b float64, xValues []float64) []float64 {
+	results := []float64{}
+	for _, x := range xValues {
+		y := CalculateY(a, b, x)
+		results = append(results, y)
+	}
+	return results
+}
+func StartLab4() {
+	a := 2.0
+	b := 4.1
+
+	fmt.Println("Задача A:")
+	xStartA := 0.77
+	xEndA := 1.77
+	stepA := 0.2
+	resultsA := SolveTaskA(a, b, xStartA, xEndA, stepA)
+	fmt.Println(resultsA)
+
+	fmt.Println("Задача B:")
+	xValuesB := []float64{1.24, 1.38, 2.38, 3.21, 0.68}
+	resultsB := SolveTaskB(a, b, xValuesB)
+	fmt.Println(resultsB)
 }
--- golang/main.go
+++ golang/main.go
@@ -6,32 +6,7 @@ import (
 	fourlaba "isuct.ru/informatics2022/4lab"
 )
 
-func solveTaskA(a, b, xStart, xEnd, step float64) {
-	for x := xStart; x <= xEnd; x += step {
-		y := fourlaba.CalculateY(a, b, x)
-		fmt.Printf("x = %.2f, y = %.5f\n", x, y)
-	}
-}
-
-func solveTaskB(a, b float64, xValues []float64) {
-	for _, x := range xValues {
-		y := fourlaba.CalculateY(a, b, x)
-		fmt.Printf("x = %.2f, y = %.5f\n", x, y)
-	}
-}
-
 func main() {
 	fmt.Println("Семенов Алексей Дмитриевич")
-	a := 2.0
-	b := 4.1
-
-	fmt.Println("Задача A:")
-	xStartA := 0.77
-	xEndA := 1.77
-	stepA := 0.2
-	solveTaskA(a, b, xStartA, xEndA, stepA)
-
-	fmt.Println("Задача B:")
-	xValuesB := []float64{1.24, 1.38, 2.38, 3.21, 0.68}
-	solveTaskB(a, b, xValuesB)
+	fourlaba.StartLab4()
 }
