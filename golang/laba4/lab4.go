fdba788a546db9c39f863fc88a5206fbf791e589
Puchkov <53227947+FOGERFOGER@users.noreply.github.com>
2024-10-19 12:06:26 +0300

    Updatelab4.go

--- golang/laba4/lab4.go
+++ golang/laba4/lab4.go
@@ -5,12 +5,12 @@ import (
 	"math"
 )
 
-func Value_Y(x float64) float64 {
+func value_Y(x float64) float64 {
 	y := (math.Pow(2.25, (math.Pow(x, 2)-1)) - math.Log10(math.Pow(x, 2)-1) + math.Cbrt((math.Pow(x, 2) - 1)))
 	return y
 }
 
-func TaskA(xstartA, xendA, stepA float64) []float64 {
+func taskA(xstartA, xendA, stepA float64) []float64 {
 	var result []float64
 
 	for x := xstartA; x < xendA; x += stepA {
@@ -20,7 +20,7 @@ func TaskA(xstartA, xendA, stepA float64) []float64 {
 	return result
 }
 
-func TaskB(Bslice []float64) []float64 {
+func taskB(Bslice []float64) []float64 {
 	var result []float64
 	for _, x := range Bslice {
 		result = append(result, Value_Y(x))
