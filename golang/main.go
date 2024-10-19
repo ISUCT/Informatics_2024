8959fe965f1c062731378b212d0026c51beea3f3
nerocos0 <rybka767676@gmail.com>
2024-10-19 21:25:11 +0300

    send tests for lab4 to folder tests

--- golang/main.go
+++ golang/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"isuct.ru/informatics2022/tests"
 	"isuct.ru/informatics2022/labs"
 )
 
@@ -9,4 +10,5 @@ func main() {
 	fmt.Println("Лутохин Никита Евгеньевич")
 	labs.Go_lab4()
 	labs.Go_lab6()
+	internal.Testing_lab4()
 }
--- golang/tests/test_for_lab4.go
+++ golang/tests/test_for_lab4.go
@@ -1,21 +1,25 @@
-package labs
+package internal
 
 import (
 	"math"
 	"testing"
+	"isuct.ru/informatics2022/labs"
 )
-
+func Testing_lab4() {
+	testing_outputY(&testing.T{})
+	testing_taskA(&testing.T{})
+	testing_taskB(&testing.T{})
+}
 func testing_outputY(t *testing.T) {
 	const const_a = 2
 	const const_b = 3
-	result_y := Return_value_y(0.11, const_a, const_b)
+	result_y := labs.Return_value_y(0.11, const_a, const_b)
 	true_res := 1.581565621572023
 	if math.IsNaN(result_y) {
 		t.Errorf("The value is a not a namber!")
 	} else if result_y != true_res {
 		t.Errorf("Returns %v, expected %v", result_y, true_res)
 	}
-
 }
 func testing_taskA(t *testing.T) {
 	y_list_true := []float64{1.581565621572023,
@@ -24,7 +28,7 @@ func testing_taskA(t *testing.T) {
 		1.620871010842551,
 		1.6372494546297607,
 		1.6540889552598357}
-	y_list_output := Task_A(2, 3)
+	y_list_output := labs.Task_A(2, 3)
 	if len(y_list_output) != 6 {
 		t.Errorf("Returns %v y, expected %v y", len(y_list_output), len(y_list_true))
 	}
@@ -40,7 +44,7 @@ func testing_taskB(t *testing.T) {
 		1.650716645162541,
 		1.6707225698630865,
 		1.7060955449869923}
-	y_list_output := Task_B(2, 3)
+	y_list_output := labs.Task_B(2, 3)
 	for i, y := range y_list_output {
 		if y != y_list_true[i] {
 			t.Errorf("The function was calculated incorrectly")
