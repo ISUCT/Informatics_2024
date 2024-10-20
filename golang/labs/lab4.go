55a6b541ee9bd97852ee14c8d5fb7061c635c9cc
qwwqqqw <pshenichnikov6002@gmail.com>
2024-10-20 11:25:15 +0300

    Update lab4.go

--- golang/labs/lab4.go
+++ golang/labs/lab4.go
@@ -13,25 +13,32 @@ func calculateY(a, b, x float64) float64 {
 	koren := math.Cbrt(x)
 	log5 := logarifm(x, 5)
 	logValue := math.Log(x - 1)
-	lgkub := math.Pow(logValue, 3)
 
+	if logValue == 0 {
+		return math.NaN()
+	}
+
+	lgkub := math.Pow(logValue, 3)
 	return (a*koren - b*log5) / lgkub
 }
 
-func taskA(a, b, xn, xk, xd float64) {
-	fmt.Println("Задача A:")
+func taskA(a, b, xn, xk, xd float64) ([]float64, []float64) {
+	var xResults, yResults []float64
 	for x := xn; x <= xk; x += xd {
 		y := calculateY(a, b, x)
-		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
+		xResults = append(xResults, x)
+		yResults = append(yResults, y)
 	}
+	return xResults, yResults
 }
 
-func taskB(a, b float64, xValues []float64) {
-	fmt.Println("\nЗадача B:")
+func taskB(a, b float64, xValues []float64) []float64 {
+	var yResults []float64
 	for _, x := range xValues {
 		y := calculateY(a, b, x)
-		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
+		yResults = append(yResults, y)
 	}
+	return yResults
 }
 
 func Runlab4() {
@@ -40,8 +47,10 @@ func Runlab4() {
 	xn := 1.5
 	xk := 3.5
 	xd := 0.4
-	taskA(a, b, xn, xk, xd)
+	xResultsA, yResultsA := taskA(a, b, xn, xk, xd)
+	fmt.Println(xResultsA, yResultsA)
 
 	xValues := []float64{1.9, 2.15, 2.34, 2.74, 3.16}
-	taskB(a, b, xValues)
+	yResultsB := taskB(a, b, xValues)
+	fmt.Println(xValues, yResultsB)
 }
