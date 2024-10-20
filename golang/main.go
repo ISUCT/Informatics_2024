f1cce26b0d0c3a3a5eefa4ca9132c867b181cc36
TheScarletLight <lemoncheit@gmailcom>
2024-10-20 13:54:12 +0300

    Сделал лабу 4

--- golang/main.go
+++ golang/main.go
@@ -2,23 +2,11 @@ package main
 
 import (
 	"fmt"
-	"isuct.ru/informatics2022/laba4"
+
+	laba4 "isuct.ru/informatics2022/lab4"
 )
 
 func main() {
-	var a float64 = 0.4
-	var b float64 = 0.8
-	var y float64
-	var x [5] float64 = [5] float64 {4.48, 3.56, 2.78, 5.28, 3.21}
-	// Задание A
-	for i := 3.2; i<3.21; i+=0.6{
-		y = laba4.Lab4(a,b,i)
-		fmt.Println(y)
-	}
-	// Задание B
-	for i := 0; i<len(x)-1; i++{
-		y = laba4.Lab4(a,b,x[i])
-		fmt.Println(y)
-	}
+	fmt.Println("Ganshevskiy_Roman_Evgenyevich")
+	laba4.Lab4()
 }
-
