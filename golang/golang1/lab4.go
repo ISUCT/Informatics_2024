25ba4dc4cdb8fa0308d62d7daffb2a2c30328ada
Dna-dat <ksyu.ryndina.00@bk.ru>
2024-10-20 16:50:07 +0300

    lab4 Рындина

--- golang/golang1/lab4.go
+++ golang/golang1/lab4.go
@@ -8,7 +8,6 @@ import (
 func ColculateFunction(a, b, x float64) float64 {
 	y := math.Pow((a+b*x)/(math.Pow(math.Log10(x), 3)), 0.2)
 	return y
-
 }
 
 func CompleteTaskA(a, b, xMin, xMax, xDelta float64) []float64 {
