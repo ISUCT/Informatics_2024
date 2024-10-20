266176b1d8c13c9023bd27478580e319083296dd
traktr1995 <egoregorkor44@gmail.com>
2024-10-20 11:57:45 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -13,6 +13,4 @@ func main() {
 	fmt.Println(lab4.TaskA(a, b, 0.11, 0.36, 0.05))
 	var s = [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}
 	fmt.Println(lab4.TaskB(a, b, s))
-=======
-	fmt.Println("Корнюшкин Егор Юрьевич")
 }
