3ce7102eb8b1c1cb8f532cd62625424184fc88f0
log69555 <143123261+log69555@users.noreply.github.com>
2024-10-21 14:57:05 +0300

    lab4 (#255)
    * я сделал 4 лабораторную
    
    * Update main.go
    
    * Delete golang/lab4/lab4.txt
    
    * я сделал
    
    * сделано
    
    ---------
    
    Co-authored-by: GypsyGang37 <bobartem04@gmail.com>
    Co-authored-by: Oleg Voitovich <40565990+FeLL1kS@users.noreply.github.com>

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/lab4"
+)
+
+const a float64 = 1.35
+const b float64 = 0.98
 
 func main() {
 	fmt.Println("Егор Шахов")
+
+	A := lab4.TaskA(1.14, 4.24, 0.62)
+	var B []float64 = []float64{0.35, 1.28, 3.51, 5.21, 4.16}
+
+	fmt.Println("Задача A:")
+	lab4.Enter(a, b, A)
+
+	fmt.Println("Задача B:")
+	lab4.Enter(a, b, B)
 }
