650acbc83ede89f65cf60ecfad07c8a2846baf87
xsssvake <svakewow@gmail.com>
2024-10-21 15:06:48 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
-
 	"isuct.ru/informatics2022/lab4"
 )
 
 func main() {
-	var res float64
-	var elements = [5]float64{2.4, 2.8, 3.9, 4.7, 3.16}
-	fmt.Println("Задача B ")
-	for _, i := range elements {
-		res = lab4.Mathfunc(7.2, 1.3, i)
-		fmt.Println("При x =", i, "функция равна: ", res)
-	}
-	fmt.Println("Задача А ")
-	var xn, xk, xdel float64 = 1.56, 4.71, 0.63
-	for i := xn; i <= xk; i += xdel {
-		fmt.Println("При x =", i, "функция равна: ", lab4.Mathfunc(7.2, 1.3, i))
-	}
+	var a float64 = 7.2
+	var b float64 = 1.3
+	var x []float64 = []float64{2.4, 2.8, 3.9, 4.7, 3.16}
+	var xn float64 = 1.56
+	var xk float64 = 4.71
+	var xdel float64 = 0.63
+
+	var resA []float64 = lab4.TaskA(a, b, xn, xk, xdel)
+	fmt.Println("Задача А", resA)
+	var resB []float64 = lab4.TaskB(a, b, x)
+	fmt.Println("Задача В", resB)
 	fmt.Println("Нерабеев Кирилл Сергеевич")
 }
