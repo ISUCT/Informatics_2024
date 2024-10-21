68df986695eb7adb284a4d921f3c6350928b419a
AHAAW <danaka2790@gmail.com>
2024-10-21 15:01:28 +0300

    lab4

--- golang/Lab4/lab4.go
+++ golang/Lab4/lab4.go
@@ -6,12 +6,13 @@ import(
 )
 func Calculate(x float64)  float64{
 	var y float64 = (math.Pow(1.2, x)) - (math.Pow(x, 1.2))
-	
 	return y
 }
 func TaskA(Xn, Xk, delX float64){
 	for x := Xn; x <= Xk; x += delX {
-		fmt.Println(Calculate(x))}
+		if math.Abs(x) >= 1{
+			fmt.Println(Calculate(x))}
+		}
 	}
 func TaskB(x [5]float64) {
 	for _, value := range x {
