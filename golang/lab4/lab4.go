fc03f794291033673ee2ce921dc2c40167a7a420
Shumskaya Daria <110334152+dipmej@users.noreply.github.com>
2024-10-26 09:08:12 +0300

    Update lab4.go

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -9,18 +9,18 @@ func Calculate(a, b, x float64) float64 {
 	return y
 }
 
-func TaskA(a, b, xbegin, xend, xdelt float64) []float64 {
-	var y []float64
+func CompleteTaskA(a, b, xbegin, xend, xdelt float64) []float64 {
+	var result []float64
 	for x := xbegin; x <= xend; x += xdelt {
-		y = append(y, Calculate(a, b, x))
+		result = append(reslt, Calculate(a, b, x))
 	}
-	return y
+	return result
 }
 
-func TaskB(a, b float64, x []float64) []float64 {
-	var t []float64
+func CompleteTaskB(a, b float64, x []float64) []float64 {
+	var result []float64
 	for i := 0; i < len(x); i++ {
-		t = append(t, Calculate(a, b, x[i]))
+		result = append(result, Calculate(a, b, x[i]))
 	}
-	return t
+	return result
 }
