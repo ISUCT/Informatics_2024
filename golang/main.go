138940406303f98d43c5fbeddbf122d3f2bce768
Shumskaya Daria <110334152+dipmej@users.noreply.github.com>
2024-10-26 08:56:14 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	var a float64 = 2.0
 	var b float64 = 0.95
-	var xb float64 = 1.25
-	var xe float64 = 2.75
-	var xd float64 = 0.3
+	var xbegin float64 = 1.25
+	var xend float64 = 2.75
+	var xdelt float64 = 0.3
 
 	var x []float64 = []float64{2.2, 3.78, 4.51, 6.58, 1.2}
 
