8260a009f5fb4836cda7ca4123ac3ee18293c1be
pulbeem <anastasiakolp22@icloud.com>
2024-10-28 21:08:38 +0300

    lab4

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,13 +1,13 @@
 package lab4
 
 import (
- "math"
+	"math"
 )
 
 func Colcul(a, b, x float64) float64 {
- if x == 0 {
-  return 0
- }
- var y float64 = math.Sqrt(float64(math.Abs(a-b*x)) / math.Pow(math.Log10(x), 3))
- return y
+	if x == 0 {
+		return 0
+	}
+	y := math.Sqrt(math.Abs(a-b*x) / math.Pow(math.Log10(x), 3))
+	return y
 }
