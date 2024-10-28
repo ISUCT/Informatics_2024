abd25f0d66a3f6f66576745688513209ba8914d2
Shumskaya Daria <110334152+dipmej@users.noreply.github.com>
2024-10-28 14:23:30 +0300

    Update lab4.go

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,8 +1,6 @@
 package lab4
 
-import (
-	"math"
-)
+import "math"
 
 func Calculate(a, b, x float64) float64 {
 	var y float64 = (1 + math.Pow(math.Log10(x/a), 2)) / (b - math.Pow(math.Exp(1), (x/a)))
@@ -12,14 +10,14 @@ func Calculate(a, b, x float64) float64 {
 func CompleteTaskA(a, b, xbegin, xend, xdelt float64) []float64 {
 	var result []float64
 	for  x := xbegin; x <= xend; x += xdelt {
-		result = append(reslt, Calculate(a, b, x))
+		result = append(result, Calculate(a, b, x))
 	}
 	return result
 }
 
 func CompleteTaskB(a, b float64, x []float64) []float64 {
 	var result []float64
-	for _, i := 0; i < len(x); idelt++ {
+	for i := 0; i < len(x); i++ {
 		result = append(result, Calculate(a, b, x[i]))
 	}
 	return result
