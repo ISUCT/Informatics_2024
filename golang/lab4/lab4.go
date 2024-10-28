e0650eaa3f2f77fc07741f8b63e067c63a712cf8
pulbeem <anastasiakolp22@icloud.com>
2024-10-28 20:40:17 +0300

    lab4

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -8,7 +8,7 @@ func Colcul (a, b, x float64) float64 {
   if x == 0 {
     return 0
   }
-  y := math.Sqrt(float64(math.Abs(a - b*x)) / math.Pow(math.Log10(x), 3))
-  
+  y float64 := math.Sqrt(float64(math.Abs(a - b*x)) / math.Pow(math.Log10(x), 3))
+
  return y;
 }
