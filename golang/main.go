1179919cb6ce7b12dbcc6f27daff186cebaa7a88
Alexander56330 <gog-aleksander.gogolev@yandex.ru>
2024-10-28 21:54:06 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -6,24 +6,20 @@ import (
 	"math"
 )
 
-
-
 func main() {
 	a := -2.5
+	xН := 3.5
+	xК := 6.5
+	deltax := 0.6
+	xValues := []float64{2.89, 3.54, 5.21, 6.28, 3.48}
 
-	fmt.Println("Задача A:")
-xН := 3.5
-xК := 6.5
-deltax := 0.6
-
+fmt.Println("Задача A:")
 for x := xН; x <= xК; x += deltax {
 	y := lab4.CalculateY(x, a)
 	fmt.Printf("x = %.2f, y = %.4f\n", x, y)
 }
 
 fmt.Println("Задача B:")
-xValues := []float64{2.89, 3.54, 5.21, 6.28, 3.48}
-
 for _, x:= range xValues {
 y := lab4.CalculateY(x, a)
 fmt.Printf("x = %.2f, y = %.4f\n", x, y)
