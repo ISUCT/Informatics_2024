57710a11ca78255c5c4981a711c0a973bfc7a207
ivyclose <blazyblazzz@gmail.com>
2024-10-29 10:04:51 +0300

    Add lab4

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -6,25 +6,30 @@ import (
 )
 
 func RunLab4() {
-	xn := 1.42
-	xk := 3.62
-	delx := 0.44
-	x1 := 1.6
-	x2 := 1.81
-	x3 := 2.24
-	x4 := 2.65
-	x5 := 3.38
-	//Задание А
-	fmt.Println("знач у для хn-хk с шагом xk:")
-	for x := xn; x <= xk; x += delx {
-		fmt.Println(y(x))
-	}
-	//Задание В
-	fmt.Println("знач у для х1-х5:", y(x1), ",", y(x2), ",", y(x3), ",", y(x4), ",", y(x5))
-}
-
-func y(x float64) float64 {
 	a := 0.8
 	b := 0.4
-	return (math.Pow(x-a, 1.0/3.0) + math.Pow(x+b, 1.0/5.0)) / (math.Pow(x, 1.0/7.0) - math.Pow(x*x-(a+b)*(a+b), 1.0/9.0))
+	fmt.Println(TaskA(1.42, 3.62, 0.44, a, b))
+	arr := []float64{1.6, 1.81, 2.24, 2.65, 3.38}
+	fmt.Println(TaskB(arr, a, b))
+}
+
+func TaskA(xn, xk, deltax, a, b float64) []float64 {
+	var yValues []float64
+	for x := xn; x <= xk; x += deltax {
+		yValues = append(yValues, Calculate_y(x, a, b))
+	}
+	return yValues
+}
+
+func TaskB(values []float64, a, b float64) []float64 {
+	var yValues []float64
+	for _, x := range values {
+		yValues = append(yValues, Calculate_y(x, a, b))
+	}
+	return yValues
+}
+
+func Calculate_y(x float64, a float64, b float64) float64 {
+	y := (math.Pow(x-a, 1.0/3.0) + math.Pow(x+b, 1.0/5.0)) / (math.Pow(x, 1.0/7.0) - math.Pow(x*x-(a+b)*(a+b), 1.0/9.0))
+	return y
 }
