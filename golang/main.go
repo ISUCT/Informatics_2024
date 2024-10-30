215f2a77b1b31b44851c73c179556e5430353a0c
Vladislav3707 <mr8815542@gmail.com>
2024-10-30 13:22:01 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"fmt"
 
-	"isuct.ru/informatics2022/lab4"
+	lab4 "isuct.ru/informatics2022/lab4"
 )
 
-const a float64 = 2.0
-const b float64 = 0.95
-
 func main() {
-	lab4.TaskA(b, a, 1.25, 2.75, 0.3
-	var p = [5]float64{2.2, 3.78, 4.51, 6.58, 1.2}
-	lab4.TaskB(a, b, p)
-
 	fmt.Println("Vanyagina_Anastasia_Olegovna")
+	lab4.RunLab4()
 }
