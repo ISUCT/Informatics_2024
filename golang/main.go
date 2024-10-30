62b4f0c5c64395c2a4a6efde5d7708286905d397
Albinka <martizflax@gmail.com>
2024-10-31 00:25:29 +0300

    Лабораторная 6

--- golang/main.go
+++ golang/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
 )
 
 func main() {
 	fmt.Println("Морозова Альбина Анатольевна")
 	lab4.CompleteLab4()
+	lab6.Lab6()
 }
