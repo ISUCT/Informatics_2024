275d68028228083d8b6c636b323fcb708fa2e1a8
traktr1995 <egoregorkor44@gmail.com>
2024-11-03 14:11:46 +0300

    Lab6

--- golang/main.go
+++ golang/main.go
@@ -3,24 +3,14 @@ package main
 import (
 	"fmt"
 
-<<<<<<< HEAD
-	"isuct.ru/informatics2022/lab4"
-)
-
-const a float64 = 2.0
-const b float64 = 3.0
-
-func main() {
-	fmt.Println(lab4.TaskA(a, b, 0.11, 0.36, 0.05))
-	var s = [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}
-	fmt.Println(lab4.TaskB(a, b, s))
-=======
 	Lab4 "isuct.ru/informatics2022/lab4"
+	Lab6 "isuct.ru/informatics2022/lab6"
 )
 
 func main() {
 	fmt.Println("Корнюшкин Егор Юрьевич")
 
 	Lab4.RunLab4()
->>>>>>> f11d5c8 (Lab4)
+
+	Lab6.RunLab6Tasks()
 }
