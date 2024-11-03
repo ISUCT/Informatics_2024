e032ea2ab11176c5bb6e1f4aaee4dc4c5af0308b
traktr1995 <egoregorkor44@gmail.com>
2024-11-03 12:48:54 +0300

    Lab4

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,10 +1,11 @@
 package lab4
 
 import (
+	"fmt"
 	"math"
 )
 
-func Calculator(a, b, x float64) float64 {
+func Calculate(a, b, x float64) float64 {
 	var y float64 = math.Asin(math.Pow(x, a)) + math.Acos(math.Pow(x, b))
 	return y
 }
@@ -12,7 +13,7 @@ func Calculator(a, b, x float64) float64 {
 func TaskA(a, b, Xn, Xk, delX float64) []float64 {
 	var Calc []float64
 	for x := Xn; x <= Xk; x += delX {
-		Calc = append(Calc, Calculator(a, b, x))
+		Calc = append(Calc, Calculate(a, b, x))
 	}
 	return Calc
 }
@@ -20,7 +21,15 @@ func TaskA(a, b, Xn, Xk, delX float64) []float64 {
 func TaskB(a float64, b float64, x [5]float64) []float64 {
 	var Calc []float64
 	for _, value := range x {
-		Calc = append(Calc, Calculator(a, b, value))
+		Calc = append(Calc, Calculate(a, b, value))
 	}
 	return Calc
+}
+
+func RunLab4() {
+	a := 2.0
+	b := 3.0
+	fmt.Println(TaskA(a, b, 0.11, 0.36, 0.05))
+	var s = [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}
+	fmt.Println(TaskB(a, b, s))
 }
--- golang/main.go
+++ golang/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+<<<<<<< HEAD
 	"isuct.ru/informatics2022/lab4"
 )
 
@@ -13,4 +14,13 @@ func main() {
 	fmt.Println(lab4.TaskA(a, b, 0.11, 0.36, 0.05))
 	var s = [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}
 	fmt.Println(lab4.TaskB(a, b, s))
+=======
+	Lab4 "isuct.ru/informatics2022/lab4"
+)
+
+func main() {
+	fmt.Println("Корнюшкин Егор Юрьевич")
+
+	Lab4.RunLab4()
+>>>>>>> f11d5c8 (Lab4)
 }
