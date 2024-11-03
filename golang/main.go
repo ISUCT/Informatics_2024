c73914c670171c75dc44f67f1efb7c58ec682c16
AHAAW <danaka2790@gmail.com>
2024-11-03 18:51:51 +0300

    Lab6

--- golang/main.go
+++ golang/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	lab6 "isuct.ru/informatics2022/Lab6"
 	"isuct.ru/informatics2022/lab4"
 )
 
 func main() {
 	fmt.Println("Головин Даниил Александрович")
 	lab4.RunLab4()
+	lab6.RunLab6()
 }
