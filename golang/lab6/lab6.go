87c4e9c70e9ce040e93d0b8b96dabd44e7347e8a
rukinapolina <polinostka@gmail.com>
2024-11-04 20:41:52 +0300

    Update lab6.go

--- golang/lab6/lab6.go
+++ golang/lab6/lab6.go
@@ -1,49 +1,44 @@
-package main
+package main 
 
-import (
- "fmt"
-)
+import ( 
+    "fmt" 
+) 
 
-type Fox struct {
- name   string
- age    int
- color  string
-}
+type Fox struct { 
+    name   string 
+    age    int 
+    color  string 
+} 
 
-func NewFox(name string, age int, color string) Fox {
- return Fox{name: name, age: age, color: color}
-}
+func NewFox(name string, age int, color string) Fox { 
+    return Fox{name: name, age: age, color: color} 
+} 
 
-func (f Fox) GetView() string {
- return 
-       /   /
-      {   + f.color +   }
-       + f.name +  is  + fmt.Sprint(f.age) +  years old.
-        __/
-    
-}
+func (f Fox) GetView() string { 
+    return fmt.Sprintf("{%s} %s is %d years old.", f.color, f.name, f.age) 
+} 
 
-func (f *Fox) SetAge(age int) {
- f.age = age
-}
+func (f *Fox) SetAge(age int) { 
+    f.age = age 
+} 
 
-func (f *Fox) SetColor(color string) {
- f.color = color
-}
+func (f *Fox) SetColor(color string) { 
+    f.color = color 
+} 
 
-func (f Fox) GetInfo() string {
- return fmt.Sprintf("Name: %s, Age: %d, Color: %s", f.name, f.age, f.color)
-}
+func (f Fox) GetInfo() string { 
+    return fmt.Sprintf("Name: %s, Age: %d, Color: %s", f.name, f.age, f.color) 
+} 
 
-func Runlab6() {
- fox := NewFox("Foxy", 3, "red")
+func Runlab6() { 
+    fox := NewFox("Foxy", 3, "red") 
 
- fmt.Println(fox.GetView())
+    fmt.Println(fox.GetView()) 
 
- fmt.Println(fox.GetInfo())
+    fmt.Println(fox.GetInfo()) 
 
- fox.SetAge(4)
- fox.SetColor("white")
+    fox.SetAge(4) 
+    fox.SetColor("white") 
 
- fmt.Println(fox.GetInfo())
+    fmt.Println(fox.GetInfo()) 
 }
