276600555c56dd6088060df914d9fc7798d9c0cf
@kekc0 <ilu4a4a@ya.ru>
2024-11-04 20:18:27 +0300

    Пофиксил названия функций и возвращение значений

--- golang/lab4/Calculate.go
+++ golang/lab4/Calculate.go
@@ -1,7 +1,6 @@
 package lab4
 
 import (
-	"fmt"
 	"math"
 )
 
@@ -9,14 +8,18 @@ func Calculate(a, b, x float64) float64 {
 	return (1 + math.Pow(math.Log10(x/a), 2)) / (b - math.Pow(2.7, x/a))
 }
 
-func F_A(a, b, Xn, Xk, dX float64) {
+func TaskA(a, b, Xn, Xk, dX float64) []float64 {
+	var yValues []float64
 	for x := Xn; x <= Xk; x += dX {
-		fmt.Println(Calculate(b, a, x))
+		yValues = append(yValues, Calculate(b, a, x))
 	}
+	return yValues
 }
 
-func F_B(b, a float64, x [5]float64) {
+func TaskB(b, a float64, x [5]float64) []float64 {
+	var yValues []float64
 	for _, value := range x {
-		fmt.Println(Calculate(b, a, value))
+		yValues = append(yValues, Calculate(b, a, value))
 	}
+	return yValues
 }
--- golang/main.go
+++ golang/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"isuct.ru/informatics2022/lab4"
 )
-
 
 func main() {
 	const a float64 = 2.0
 	const b float64 = 0.95
-	lab4.F_A(b, a, 1.25, 2.75, 0.3)
 	var p = [5]float64{2.2, 3.78, 4.51, 6.58, 1.2}
-	lab4.F_B(a, b, p)
+
+	fmt.Println("Щербаков Илья Алексадрович")
+	fmt.Println("Task А:\n", lab4.TaskA(b, a, 1.25, 2.75, 0.3))
+	fmt.Println("Task В:\n", lab4.TaskB(a, b, p))
 }
