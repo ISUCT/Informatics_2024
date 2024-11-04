cdc9dd2d18091d828b28456792cb8b19a41acb9a
xakristl8 <xakristl8@bk.ru>
2024-11-04 14:04:06 +0300

    удаление ненужных папок и испровление ошибки с библеотекой для тестирования

--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,15 @@
 package main
 
-import lab6 "isuct.ru/informatics2022/laba6"
+import (
+	"isuct.ru/informatics2022/todo"
+	"isuct.ru/informatics2022/todo/console"
+)
 
 func main() {
-	// var list todo.Todo
+	var list todo.Todo
 
-	// list.AddTask(console.InputTask())
-	// list.AddTask(console.InputTask())
+	list.AddTask(console.InputTask())
+	list.AddTask(console.InputTask())
 
-	// list.OutputTodo()
-	lab6.Laba6()
+	list.OutputTodo()
 }
