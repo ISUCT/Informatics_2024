49cc87b41959651e65d9e9e00e55b68e9f4f1773
1 <1@yandex.ru>
2024-11-05 09:57:23 +0300

    Added lab 4 and 6

--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-
+	
+	"isuct.ru/informatics2022/lab6"
 	lab4 "isuct.ru/informatics2022/lab4"
 )
 
 func main() {
 	fmt.Println("Vanyagina_Anastasia_Olegovna")
 	lab4.RunLab4()
+	lab6.RunLab6()
 }
