6579cb87ccbfde3a6cd5299c6b53977fbca4839d
Kiruhabri3 <druzininkirill15@gmail.com>
2024-11-06 23:16:49 +0300

    laba4

--- golang/main.go
+++ golang/main.go
@@ -1,11 +1,13 @@
-package main
+ackage main
 
 import (
  "fmt"
     lab6 "isuct.ru/informatics2022/laba6"
+ laba4 "isuct.ru/informatics2022/labs/laba4"
 )
 
 func main() {
  fmt.Println("Дружинин Кирилл Александрович")
+ laba4.Y_B()
  lab6.Laba6Run()
 }
