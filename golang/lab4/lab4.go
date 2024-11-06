797161fb6cbbf772fdd6146ee5c22333ae0b13ad
Kirill <svakewow@gmail.com>
2024-11-06 20:37:05 +0300

    fix lab-4

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,16 +1,19 @@
 package lab4
+
 import (
+	"fmt"
 	"math"
 )
+
 func Calculate(a, b, elements float64) float64 {
 	y := math.Pow((a+b*elements)/(math.Pow(math.Log10(elements), 3)), 0.2)
 	return y
 }
 
-func TaskA(a, b, xn, xk, xdel float64) []float64{
+func TaskA(a, b, xn, xk, xdel float64) []float64 {
 	var res []float64
 	for i := xn; i < xk; i += xdel {
-		res = append(res, Calculate(a,b,i))
+		res = append(res, Calculate(a, b, i))
 	}
 	return res
 }
@@ -21,4 +24,18 @@ func TaskB(a, b float64, x []float64) []float64 {
 		res = append(res, Calculate(a, b, i))
 	}
 	return res
+}
+
+func Lab4() {
+	var a float64 = 7.2
+	var b float64 = 1.3
+	var x []float64 = []float64{2.4, 2.8, 3.9, 4.7, 3.16}
+	var xn float64 = 1.56
+	var xk float64 = 4.71
+	var xdel float64 = 0.63
+
+	var resA []float64 = TaskA(a, b, xn, xk, xdel)
+	fmt.Println("Задача А", resA)
+	var resB []float64 = TaskB(a, b, x)
+	fmt.Println("Задача В", resB)
 }
