d6670f54b8415f0713b10587f5e6fc12fc831357
AHAAW <danaka2790@gmail.com>
2024-11-06 12:43:00 +0300

    Исправил

--- golang/Lab4/lab4.go
+++ golang/Lab4/lab4.go
@@ -1,21 +1,33 @@
 package lab4
 
-import(
-	"math"
+import (
 	"fmt"
+	"math"
 )
-func Calculate(x float64)  float64{
-	var y float64 = (math.Pow(1.2, x)) - (math.Pow(x, 1.2))
+
+func Calculate(x float64) float64 {
+	var y float64 = math.Acos(x)
+	if math.Abs(x) >= 1 {
+		y = (math.Pow(1.2, x)) - (math.Pow(x, 1.2))
+	}
 	return y
 }
-func TaskA(Xn, Xk, delX float64){
-	for x := Xn; x <= Xk; x += delX {
-		if math.Abs(x) >= 1{
-			fmt.Println(Calculate(x))}
-		}
+
+func TaskA(Xmin, Xmax, Xdel float64) []float64 {
+	var y []float64
+	for x := Xmin; x <= Xmax; x += Xdel {
+		y = append(y, Calculate(x))
 	}
-func TaskB(x [5]float64) {
+	return y
+}
+func TaskB(x [5]float64) []float64 {
+	var y []float64
 	for _, value := range x {
-		fmt.Println(Calculate(value))
+		y = append(y, Calculate(value))
 	}
+	return y
+}
+func RunLab4() {
+	fmt.Println(TaskA(0.2, 2.2, 0.4))
+	fmt.Println(TaskB([5]float64{0.1, 0.9, 1.2, 1.5, 1.3}))
 }
--- golang/main.go
+++ golang/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	lab6 "isuct.ru/informatics2022/Lab6"
 	"isuct.ru/informatics2022/lab4"
 )
 
 func main() {
-	/*Lab3*/
 	fmt.Println("Головин Даниил Александрович")
-	/*Lab4*/
-	/*TaskA*/lab4.TaskA(0.2, 2.2, 0.4)
-	/*TaskB*/var p = [5]float64{0.1, 0.9, 1.2, 1.5, 2.3}
-	lab4.TaskB(p)
+	lab4.RunLab4()
+	lab6.RunLab6()
 }
-	
