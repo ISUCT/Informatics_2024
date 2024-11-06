6c9c1aa0b256e8928907e6a15e41b5e7cf095888
dusyaT <titovadasa21@gmail.com>
2024-11-06 17:50:38 +0300

    Update lab4.go

--- golang/labs/lab4/lab4.go
+++ golang/labs/lab4/lab4.go
@@ -13,23 +13,22 @@ func Lab4A(xn, xk, dx float64) float64 {
 	return 0
 }
 
-func Lab4B(x1, x2, x3, x4, x5 float64) float64 {
+func Lab4B(xv []float64) float64 {
 	fmt.Println("Задача B")
-	fmt.Printf("y(%f) = %f\n", x1, Lab4Y(x1))
-	fmt.Printf("y(%f) = %f\n", x2, Lab4Y(x2))
-	fmt.Printf("y(%f) = %f\n", x3, Lab4Y(x3))
-	fmt.Printf("y(%f) = %f\n", x4, Lab4Y(x4))
-	fmt.Printf("y(%f) = %f\n", x5, Lab4Y(x5))
+	for i := 0; i<len(xv); i++ {
+		fmt.Printf("y(%f) = %f\n", xv[i], Lab4Y(xv[i]))
+	}
 	return 0
 }
 
 func Lab4Y(x float64) float64 {
-	return (math.Pow(math.Pow(math.Asin(x), 4)+math.Pow(math.Acos(x), 6), 1.0/7.0))
+	var DegreeAsin, DegreeAcos,DegreeNumerator, DegreeDenumenator float64 = 4, 6, 1, 7
+	return (math.Pow(math.Pow(math.Asin(x), DegreeAsin)+math.Pow(math.Acos(x), DegreeAcos), DegreeNumerator/DegreeDenumenator))
 }
 
 func Lab4() {
 	var xn, xk, dx float64 = 0.22, 0.92, 0.14
-	var x1, x2, x3, x4, x5 float64 = 0.1, 0.35, 0.4, 0.55, 0.6
+	var xv []float64 = []float64{0.1, 0.35, 0.4, 0.55, 0.6}
 	Lab4A(xn, xk, dx)
-	Lab4B(x1, x2, x3, x4, x5)
+	Lab4B(xv)
 }
