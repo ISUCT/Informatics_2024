e226fd57a9925e305142993f20434530d8aacfe3
ruslan709 <ruslan9644915858@yandex.ru>
2024-11-08 15:25:38 +0300

    fixed

--- golang/labs/lab7.go
+++ golang/labs/lab7.go
@@ -1,11 +1,12 @@
 package labs
 
 import (
+	"errors"
 	"fmt"
 )
 
 type Product interface {
-	sale(discount float64)
+	sale(discount float64) error
 	price() float64
 	productFeature() string
 }
@@ -17,8 +18,18 @@ type Electronics struct {
 	Model string
 }
 
-func (e *Electronics) sale(discount float64) {
+func (e *Electronics) sale(discount float64) error {
+	if e.Name == "" || e.Brand == "" || e.Model == "" {
+		return errors.New("Не все поля заполнены для Electronics")
+	}
+	if discount < 0 {
+		return errors.New("Скидка не может быть меньше 0")
+	}
+	if discount > 100 {
+		return errors.New("Скидка не может больше 100%")
+	}
 	e.Price = e.Price * (1 - discount/100)
+	return nil
 }
 
 func (e *Electronics) price() float64 {
@@ -26,6 +37,9 @@ func (e *Electronics) price() float64 {
 }
 
 func (e *Electronics) productFeature() string {
+	if e.Name == "" || e.Brand == "" || e.Model == "" {
+		return ("Не все поля заполнены!!!")
+	}
 	return fmt.Sprintf("Name: %s, Brand: %s, Model: %s, Price: %.2f", e.Name, e.Brand, e.Model, e.Price)
 }
 
@@ -36,8 +50,18 @@ type Clothing struct {
 	Color string
 }
 
-func (c *Clothing) sale(discount float64) {
+func (c *Clothing) sale(discount float64) error {
+	if c.Name == "" || c.Size == "" || c.Color == "" {
+		return errors.New("Не все поля заполнены для Clothing")
+	}
+	if discount < 0 {
+		return errors.New("Скидка не может быть меньше 0")
+	}
+	if discount > 100 {
+		return errors.New("Скидка не может больше 100%")
+	}
 	c.Price = c.Price * (1 - discount/100)
+	return nil
 }
 
 func (c *Clothing) price() float64 {
@@ -45,6 +69,9 @@ func (c *Clothing) price() float64 {
 }
 
 func (c *Clothing) productFeature() string {
+	if c.Name == "" || c.Size == "" || c.Color == "" {
+		return ("Не все поля заполнены!!!")
+	}
 	return fmt.Sprintf("Name: %s, Size: %s, Color: %s, Price: %.2f", c.Name, c.Size, c.Color, c.Price)
 }
 
@@ -54,8 +81,18 @@ type Food struct {
 	Weight float64
 }
 
-func (f *Food) sale(discount float64) {
+func (f *Food) sale(discount float64) error {
+	if f.Name == "" {
+		return errors.New("Не все поля заполнены для Food")
+	}
+	if discount < 0 {
+		return errors.New("Скидка не может быть меньше 0")
+	}
+	if discount > 100 {
+		return errors.New("Скидка не может больше 100%")
+	}
 	f.Price = f.Price * (1 - discount/100)
+	return nil
 }
 
 func (f *Food) price() float64 {
@@ -63,6 +100,9 @@ func (f *Food) price() float64 {
 }
 
 func (f *Food) productFeature() string {
+	if f.Name == "" {
+		return ("Не все поля заполнены!!!")
+	}
 	return fmt.Sprintf("Name: %s, Weight: %.2f kg, Price: %.2f", f.Name, f.Weight, f.Price)
 }
 
@@ -76,11 +116,20 @@ func GeneralPrice(products []Product) float64 {
 
 func RunLab7() {
 	product1 := &Electronics{Name: "Телефон", Price: 20, Brand: "Iphone", Model: "5s"}
-	product1.sale(25)
+	if err := product1.sale(25); err != nil {
+		fmt.Println(err)
+		return
+	}
 	product2 := &Clothing{Name: "Рубашка", Price: 14.32, Size: "L", Color: "white"}
-	product2.sale(10)
+	if err := product2.sale(10); err != nil {
+		fmt.Println(err)
+		return
+	}
 	product3 := &Food{Name: "Яблоко", Price: 2.35, Weight: 1}
-	product3.sale(15)
+	if err := product3.sale(15); err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	products := []Product{product1, product2, product3}
 
