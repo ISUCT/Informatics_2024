22f4abc9cb872be62703749fedbd3e3e0058f1af
@kekc0 <ilu4a4a@ya.ru>
2024-11-08 20:27:06 +0300

    Исправил main.go

--- golang/main.go
+++ golang/main.go
@@ -7,11 +7,7 @@ import (
 )
 
 func main() {
-	const a float64 = 2.0
-	const b float64 = 0.95
-	var p = [5]float64{2.2, 3.78, 4.51, 6.58, 1.2}
-
 	fmt.Println("Щербаков Илья Алексадрович")
-	fmt.Println("Task А:\n", lab4.TaskA(b, a, 1.25, 2.75, 0.3))
-	fmt.Println("Task В:\n", lab4.TaskB(a, b, p))
+	fmt.Println("Task А:\n", lab4.TaskA(0.95, 2.0, 1.25, 2.75, 0.3))
+	fmt.Println("Task В:\n", lab4.TaskB(2.0, 0.95, [5]float64{2.2, 3.78, 4.51, 6.58, 1.2}))
 }
