38e4d79f450ad6434ede651921ffca04332cb43c
xakristl8 <xakristl8@bk.ru>
2024-11-09 22:57:12 +0300

    определенны enums, добавленны глобальные переменные для ошибок

--- golang/lab7/furniture.go
+++ golang/lab7/furniture.go
@@ -2,22 +2,33 @@ package lab7
 
 import "errors"
 
+var ErrMaterial error = errors.New("invalid material")
+
+type Material string
+
+const (
+	Skin    Material = "кожа"
+	Wood    Material = "дерево"
+	Plywood Material = "Фанера"
+	Dsp     Material = "дсп"
+)
+
 type furniture struct {
 	product
-	material string
+	material Material
 }
 
-func (f *furniture) SetMaterial(material string) error {
-	for _, standardmaterial := range []string{"кожа", "дерево", "Фанера", "дсп"} {
+func (f *furniture) SetMaterial(material Material) error {
+	for _, standardmaterial := range []Material{Skin, Wood, Plywood, Dsp} {
 		if material == standardmaterial {
 			f.material = material
 			return nil
 		}
 	}
-	return errors.New("invalid material")
+	return ErrMaterial
 }
 
-func NewFurniture(id int, name string, price float64, material string) *furniture {
+func NewFurniture(id int, name string, price float64, material Material) *furniture {
 	f := &furniture{
 		product: newProduct(id, name, price),
 	}
--- golang/lab7/lab7.go
+++ golang/lab7/lab7.go
@@ -8,9 +8,9 @@ import (
 func CompleteLab7() {
 	var err error
 
-	bentli := NewCar(0, "бентли", 100000, "красный", 200)
-	sofa := NewFurniture(1, "диван", 1000, "кожа")
-	chear := NewFurniture(2, "стул", 10, "дерево")
+	bentli := NewCar(0, "бентли", 100000, Red, 200)
+	sofa := NewFurniture(1, "диван", 1000, Skin)
+	chear := NewFurniture(2, "стул", 10, Wood)
 	cucumber := Newfood(3, "огурец", 5)
 
 	var ProductList []Product = []Product{
@@ -29,11 +29,11 @@ func CompleteLab7() {
 	}
 	fmt.Printf("общяя стоимость товара после скидок в 10 процентов: %v \n", GetTotalPrice(ProductList))
 
-	err = bentli.SetColor("белый")
+	err = bentli.SetColor(White)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = sofa.SetMaterial("Фанера")
+	err = sofa.SetMaterial(Plywood)
 	if err != nil {
 		log.Fatal(err)
 	}
