f79478b0057c2f17ca456ff50b022432a1113e44
satinamik <puryshev3017@gmail.com>
2024-11-13 16:33:28 +0300

    Исправление лабы 4 и Реализация структуры персонаж игры в лабе 6

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -5,34 +5,42 @@ import (
 	"math"
 )
 
-func yFunc(x float64) float64 {
+func Calculation(x float64) float64 {
 	return math.Pow(math.Sqrt(math.Asin(x)*math.Asin(x)*math.Asin(x)*math.Asin(x)+math.Acos(x)*math.Acos(x)*math.Acos(x)*math.Acos(x)), 1.0/7.0)
 }
 
 func Laba() {
-	// Задача A: Найти значения функции y для xH - xK с шагом x_delta
 	xH := 0.22
 	xK := 0.92
-	x_delta := 0.14
+	x_Delta := 0.14
+	resultsA := calculateYValuesForRange(xH, xK, x_Delta)
+	header := fmt.Sprintf("Значения y для x от %.2f до %.2f c шагом %.2f", xH, xK, x_Delta)
+	printResults(header, xH, xK, resultsA)
+
+	xs := []float64{0.1, 0.35, 0.4, 0.55, 0.6}
+	resultsB := calculateYValuesForXList(xs)
+	printResults(fmt.Sprintf("Значения y для x от %f до %f", xs[0], xs[len(xs)-1]), xs[0], xs[len(xs)-1], resultsB)
+}
+
+func calculateYValuesForRange(xH, xK, xDelta float64) []float64 {
 	var results []float64
-
-	for x := xH; x <= xK; x += x_delta {
-		results = append(results, yFunc(x))
+	for x := xH; x <= xK; x += xDelta {
+		results = append(results, Calculation(x))
 	}
+	return results
+}
 
-	fmt.Println("Значения y для x от", xH, "до", xK, "c шагом", x_delta)
+func calculateYValuesForXList(xs []float64) []float64 {
+	var results []float64
+	for _, x := range xs {
+		results = append(results, Calculation(x))
+	}
+	return results
+}
+func printResults(header string, startValue interface{}, endValue interface{}, results []float64) {
+	fmt.Println(header, startValue, "до", endValue)
 	fmt.Println("Результаты:")
 	for _, result := range results {
 		fmt.Printf("%v\n", result)
-	}
-
-	// Задача B: Найти значения функции для заданных x
-	xs := []float64{0.1, 0.35, 0.4, 0.55, 0.6}
-
-	fmt.Println("Значения y для x от", xs[0], "до", xs[len(xs)-1])
-	fmt.Println("Результаты:")
-	for _, x := range xs {
-		res := yFunc(x)
-		fmt.Printf("%v\n", res)
 	}
 }
--- golang/main.go
+++ golang/main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 
 	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
 )
 
 func main() {
 	fmt.Print("Пурышев Владислав Витальевич")
+
 	lab4.Laba()
+
+	lab6.Completelaba()
+
 }
