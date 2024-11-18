a8c97874d12ef99d00bc174460a180702a514a4d
alisa_chin <95969023+alica64@users.noreply.github.com>
2024-11-18 19:43:22 +0300

    Update furniture.go

--- golang/lab7/furniture.go
+++ golang/lab7/furniture.go
@@ -1,9 +1,9 @@
 package lab7
 
 type Furniture struct {
-	price         float64
-	tupefurniture string
-	material      string
+	price    float64
+	name     string
+	material string
 }
 
 func (f *Furniture) getPrice() float64 {
@@ -14,11 +14,18 @@ func (f *Furniture) setPrice(newPrice float64) {
 	f.price = newPrice
 }
 
-func (f *Furniture) discount(discount float64) {
+func (f *Furniture) applyDiscount(discount float64) {
 	f.price = f.price * (100 - discount) / 100
 }
 
-func (f *Furniture) setCharacteristics(newTupefurniture string, newMaterial string) {
-	f.tupefurniture = newTupefurniture
+func (f *Furniture) getName() string {
+	return f.name
+}
+
+func (f *Furniture) setName(newName string) {
+	f.name = newName
+}
+
+func (f *Furniture) setMaterial(newMaterial string) {
 	f.material = newMaterial
 }
