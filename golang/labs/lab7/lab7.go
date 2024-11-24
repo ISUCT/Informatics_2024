145aeba4c3284a3596b92d2b1cc1c8b7c6f31838
Milanaaa0 <milanchik100@mail.ru>
2024-11-24 16:10:41 +0300

    update

--- golang/labs/lab7/lab7.go
+++ golang/labs/lab7/lab7.go
@@ -11,10 +11,10 @@ func CalculateProductsSum(products []Product) float64 {
 }
 
 func RunLab7() {
-	food := &Food{50.15, "картошка", "вкусвилл", 17}
-	cosmetics := &Cosmetics{"помада", 1700, "Chanel"}
-	clothes := &Clothes{"толстовка", 3000.99, "Levi`s", "хлопок", "осень"}
-	products := []Product{food, cosmetics, clothes}
+	potato := &Food{50.15, "картошка", "вкусвилл", 17}
+	lipstick := &Cosmetics{"помада", 1700, "Chanel"}
+	sweatshirt := &Clothes{"толстовка", 3000.99, "Levi`s", "хлопок", "осень"}
+	products := []Product{potato, lipstick, sweatshirt}
 	fmt.Println("Товары")
 	fmt.Println("Общая стоимость:", CalculateProductsSum(products), "рублей")
 	for _, product := range products {
@@ -26,9 +26,8 @@ func RunLab7() {
 	}
 	fmt.Println("Общая стоимость товаров после применения скидки 20%:", CalculateProductsSum(products), "рублей")
 
-	fmt.Println("Информация про товар clothes")
-	fmt.Println("название:", clothes.getName())
-	fmt.Println("материал:", clothes.getMaterial())
-	fmt.Println("сезон:", clothes.getSeason())
-	fmt.Println("бренд:", clothes.getBrand())
+	fmt.Println("Информация про товар толстовка")
+	fmt.Println("материал:", sweatshirt.getMaterial())
+	fmt.Println("сезон:", sweatshirt.getSeason())
+	fmt.Println("бренд:", sweatshirt.getBrand())
 }
