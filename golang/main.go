de01b8e4e94e88d98812768751067cef2f5eb74e
ruslan709 <ruslan9644915858@yandex.ru>
2024-11-25 22:17:42 +0300

    fixed

--- golang/main.go
+++ golang/main.go
@@ -5,13 +5,11 @@ import (
 
 	"isuct.ru/informatics2022/labs/lab4"
 	"isuct.ru/informatics2022/labs/lab6"
-	"isuct.ru/informatics2022/labs/lab7"
 	"isuct.ru/informatics2022/labs/lab8"
 )
 
 func main() {
 	fmt.Println("Kulikov Ruslan Igorevich")
-	lab7.RunLab7()
 	lab8.RunFileLab()
 	lab4.RunLab4()
 	lab6.RunLab6()
