28c1c95628f119e71701b4e806a03c4bb5d31d49
ruslan709 <ruslan9644915858@yandex.ru>
2024-11-25 22:06:26 +0300

    fixed

--- golang/main.go
+++ golang/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"isuct.ru/informatics2022/labs/lab4"
+	"isuct.ru/informatics2022/labs/lab6"
 	"isuct.ru/informatics2022/labs/lab7"
 	"isuct.ru/informatics2022/labs/lab8"
 )
@@ -13,4 +14,5 @@ func main() {
 	lab7.RunLab7()
 	lab8.RunFileLab()
 	lab4.RunLab4()
+	lab6.RunLab6()
 }
