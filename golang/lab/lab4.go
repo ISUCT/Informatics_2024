dec13a5a2f30900079c646b55d51aa2aa55bed11
Dmitriy Belov <dimabelov2006@gmail.com>
2024-11-27 00:44:20 +0300

    Added lab8

--- golang/lab/lab4.go
+++ golang/lab/lab4.go
@@ -2,11 +2,11 @@ package lab
 
 import (
 	"fmt"
-	"math"
 	"io"
+	"math"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 func Calculate(b, x float64) float64 {
@@ -16,17 +16,17 @@ func Calculate(b, x float64) float64 {
 func NumbersTaskA() []string {
 	var str_numbers []string
 	file, err := os.Open("lab4A.txt")
-	if err != nil{
-        fmt.Println("Unable to create file:", err) 
-        os.Exit(1)
+	if err != nil {
+		fmt.Println("Unable to create file:", err)
+		os.Exit(1)
 	}
 
 	data := make([]byte, 64)
-	
-	for{
-        n, err := file.Read(data)
-        if err == io.EOF{
-            break
+
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF {
+			break
 		}
 		str_numbers = strings.Split(string(data[:n]), ",")
 	}
@@ -44,17 +44,17 @@ func TaskA(b, Xn, Xk, delX float64) []float64 {
 func NumbersTaskB() []string {
 	var str_numbers []string
 	file, err := os.Open("lab4B.txt")
-	if err != nil{
-        fmt.Println("Unable to create file:", err) 
-        os.Exit(1)
+	if err != nil {
+		fmt.Println("Unable to create file:", err)
+		os.Exit(1)
 	}
 
 	data := make([]byte, 64)
-	
-	for{
-        n, err := file.Read(data)
-        if err == io.EOF{
-            break
+
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF {
+			break
 		}
 		str_numbers = strings.Split(string(data[:n]), ",")
 	}
@@ -69,7 +69,7 @@ func TaskB(b float64, x []float64) []float64 {
 	return arr
 }
 
-func PrintValue (Values []float64) {
+func PrintValue(Values []float64) {
 	for _, value := range Values {
 		fmt.Println(value)
 	}
@@ -78,7 +78,7 @@ func PrintValue (Values []float64) {
 func RunLab4Tasks() {
 	RunLab8Tasks()
 	const b float64 = 2.5
-	
+
 	var arr []string
 	arr = NumbersTaskA()
 	Xn, _ := strconv.ParseFloat(arr[0], 64)
@@ -90,11 +90,11 @@ func RunLab4Tasks() {
 	var slice []float64
 	for _, value := range arr {
 		if value != "" {
-		number, err := strconv.ParseFloat(value, 64)
-		if err != nil {
-			panic(err)
-		}
-		slice = append(slice, number)
+			number, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				panic(err)
+			}
+			slice = append(slice, number)
 		}
 	}
 	ValuesB := TaskB(b, slice)
