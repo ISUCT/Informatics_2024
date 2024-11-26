478dccb0f311e0c1d9983c9ed15187f7058b5dd4
Dmitriy Belov <dimabelov2006@gmail.com>
2024-11-27 00:43:45 +0300

    Added lab8

--- golang/lab/lab4.go
+++ golang/lab/lab4.go
@@ -3,10 +3,34 @@ package lab
 import (
 	"fmt"
 	"math"
+	"io"
+	"os"
+	"strings"
+	"strconv"
 )
 
 func Calculate(b, x float64) float64 {
 	return (1 + math.Pow(math.Sin(math.Pow(b, 3)+math.Pow(x, 3)), 2)) / (math.Cbrt(math.Pow(b, 3) + math.Pow(x, 3)))
+}
+
+func NumbersTaskA() []string {
+	var str_numbers []string
+	file, err := os.Open("lab4A.txt")
+	if err != nil{
+        fmt.Println("Unable to create file:", err) 
+        os.Exit(1)
+	}
+
+	data := make([]byte, 64)
+	
+	for{
+        n, err := file.Read(data)
+        if err == io.EOF{
+            break
+		}
+		str_numbers = strings.Split(string(data[:n]), ",")
+	}
+	return str_numbers
 }
 
 func TaskA(b, Xn, Xk, delX float64) []float64 {
@@ -15,6 +39,26 @@ func TaskA(b, Xn, Xk, delX float64) []float64 {
 		arr = append(arr, Calculate(b, x))
 	}
 	return arr
+}
+
+func NumbersTaskB() []string {
+	var str_numbers []string
+	file, err := os.Open("lab4B.txt")
+	if err != nil{
+        fmt.Println("Unable to create file:", err) 
+        os.Exit(1)
+	}
+
+	data := make([]byte, 64)
+	
+	for{
+        n, err := file.Read(data)
+        if err == io.EOF{
+            break
+		}
+		str_numbers = strings.Split(string(data[:n]), ",")
+	}
+	return str_numbers
 }
 
 func TaskB(b float64, x []float64) []float64 {
@@ -32,10 +76,29 @@ func PrintValue (Values []float64) {
 }
 
 func RunLab4Tasks() {
+	RunLab8Tasks()
 	const b float64 = 2.5
-	var slice = []float64{1.1, 2.4, 3.6, 1.7, 3.9}
-	ValuesA := TaskA(b, 1.28, 3.28, 0.4)
+	
+	var arr []string
+	arr = NumbersTaskA()
+	Xn, _ := strconv.ParseFloat(arr[0], 64)
+	Xk, _ := strconv.ParseFloat(arr[1], 64)
+	delX, _ := strconv.ParseFloat(arr[2], 64)
+	ValuesA := TaskA(b, Xn, Xk, delX)
+
+	arr = NumbersTaskB()
+	var slice []float64
+	for _, value := range arr {
+		if value != "" {
+		number, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			panic(err)
+		}
+		slice = append(slice, number)
+		}
+	}
 	ValuesB := TaskB(b, slice)
+
 	PrintValue(ValuesA)
 	PrintValue(ValuesB)
 }
