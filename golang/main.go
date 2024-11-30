4d197eb1e534dc8a2bbd5008faffe8ea23a4110b
Milanaaa0 <milanchik100@mail.ru>
2024-11-30 17:49:16 +0300

    решаю конфликты

--- golang/main.go
+++ golang/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+<<<<<<< Updated upstream
 	"isuct.ru/informatics2022/labs"
 	lab "isuct.ru/informatics2022/labs/lab7"
 )
@@ -10,4 +11,20 @@ func main() {
 	labs.RunLab6()
 	lab.RunLab7()
 	labs.RunLab8()
+=======
+	"fmt"
+
+	lab4 "isuct.ru/informatics2022/labs/lab4"
+	lab6 "isuct.ru/informatics2022/labs/lab6"
+	lab7 "isuct.ru/informatics2022/labs/lab7"
+	lab8 "isuct.ru/informatics2022/labs/lab8"
+)
+
+func main() {
+	fmt.Println("Панова Милана Седатовна")
+	lab4.RunLab4()
+	lab6.RunLab6()
+	lab7.RunLab7()
+	lab8.RunLab8()
+>>>>>>> Stashed changes
 }
