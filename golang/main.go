f238b29df5ea8b492f2b83523f96aadd119578fa
Milanaaa0 <milanchik100@mail.ru>
2024-11-30 18:22:54 +0300

    .

--- golang/main.go
+++ golang/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	lab4 "isuct.ru/informatics2022/labs/package_lab4"
 	lab6 "isuct.ru/informatics2022/labs/package_lab6"
 	lab7 "isuct.ru/informatics2022/labs/package_lab7"
@@ -8,6 +10,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Панова Милана Седатовна")
 	lab4.RunLab4()
 	lab6.RunLab6()
 	lab7.RunLab7()
