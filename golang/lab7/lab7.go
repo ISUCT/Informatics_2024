c1fae0e51a3408e87827dddd2abdbab27111672b
Alexander56330 <gog-aleksander.gogolev@yandex.ru>
2024-12-04 21:56:59 +0300

    Update lab7.go

--- golang/lab7/lab7.go
+++ golang/lab7/lab7.go
@@ -1,1 +1,42 @@
+package lab7
 
+import (
+	"fmt"
+	"log"
+)
+
+func CompleteLab7() {
+	var err error
+
+	priora := Newbibika(0, "анаконда", 100000, Gold, 200)
+	sofa := Newmebel(1, "диван", 1000, Skin)
+	armchear := Newmebel(2, "кресло", 10, Wood)
+	shaurma := Newvkusnyshki(3, "шаурма", 5)
+
+	var ProductList []Product = []Product{
+		priora,
+		sofa,
+		armchear,
+		shaurma,
+	}
+
+	fmt.Printf("общяя стоимость товара до скидок: %v \n", GetTotalPrice(ProductList))
+	for _, product := range ProductList {
+		err = product.SetDiscount(10)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Printf("общяя стоимость товара после скидок в 10 процентов: %v \n", GetTotalPrice(ProductList))
+
+	err = priora.SetColor(Black)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sofa.SetMaterial(Diamonds)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	displayListProducts(ProductList)
+}
