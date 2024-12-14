882b48db39ac867b8f44271d76efea7d3d7d3cda
Yuno121 <98771348+Yuno121@users.noreply.github.com>
2024-12-14 21:18:58 +0300

    ТОЧНО ТОЧНО финальное изменения 4 лабы

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -6,7 +6,6 @@ import (
 )
 
 var a float64 = 1.6
-var imin, imax, razn, value float64
 
 func lab4(a, x float64) float64 {
 	return (math.Pow(a, math.Pow(x, 2)-1) - math.Log10(math.Pow(x, 2)-1) + math.Cbrt(math.Pow(x, 2)-1))
@@ -16,14 +15,14 @@ func lab4A(imin float64, imax float64, razn float64) {
 		fmt.Println(lab4(a, i))
 	}
 }
-func lab4B() {
-	var x = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
+func lab4B(x [5]float64) {
 	for _, value := range x {
 		fmt.Println(lab4(a, value))
 	}
 }
 func Lab4AB() {
+	var x = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
 	lab4A(1.2, 3.7, 0.5)
-	lab4B()
+	lab4B(x)
 }
 
