888ed6256ce2765d66187423ad9b00fe9ff369d0
Yuno121 <98771348+Yuno121@users.noreply.github.com>
2024-12-14 21:08:33 +0300

    полностью исправил 4 лабу

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,23 +1,29 @@
 package lab4
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 var a float64 = 1.6
-func Lab4(a, x float64) float64 {
+var imin, imax, razn, value float64
+
+func lab4(a, x float64) float64 {
 	return (math.Pow(a, math.Pow(x, 2)-1) - math.Log10(math.Pow(x, 2)-1) + math.Cbrt(math.Pow(x, 2)-1))
 }
-func lab4A(imin, imax, razn) {
-	for imin; imin < imax; imin += razn {
-		fmt.Println(lab4.Lab4(a, i))
+func lab4A(imin float64, imax float64, razn float64) {
+	for i := imin; i < imax; i += razn {
+		fmt.Println(lab4(a, i))
 	}
 }
 func lab4B() {
-	var x [5]float64 = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
-		for index, value := range x {
-			fmt.Println(lab4.Lab4(a, x[i]))
-		}
+	var x = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
+	for _, value := range x {
+		fmt.Println(lab4(a, value))
+	}
 }
-func Lab4AB(){
-	fmt.Println(lab4.lab4A(1.2, 3.7, 0.5))
-	fmt.Println(lab4.lab4B())
+func Lab4AB() {
+	lab4A(1.2, 3.7, 0.5)
+	lab4B()
 }
+
