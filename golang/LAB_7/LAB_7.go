617002c9f3f28a6271b5c5116c94a80a3fad3a2f
дадая дадая <unwiser.png@gmail.com>
2024-12-14 09:07:29 +0300

    lab_7 fix

--- golang/LAB_7/LAB_7.go
+++ golang/LAB_7/LAB_7.go
@@ -6,51 +6,7 @@ type Product interface {
 	GetName() string
 	GetPrice() float64
 	SetPrice(price float64)
-	Discount(discount float64)
-}
-
-type Laptop struct {
-	Name  string
-	Price float64
-	Brand string
-}
-
-func (l *Laptop) GetName() string {
-	return l.Name
-}
-
-func (l *Laptop) GetPrice() float64 {
-	return l.Price
-}
-
-func (l *Laptop) SetPrice(price float64) {
-	l.Price = price
-}
-
-func (l *Laptop) Discount(discount float64) {
-	l.Price -= l.Price * discount / 100
-}
-
-type Car struct {
-	Name  string
-	Price float64
-	Brand string
-}
-
-func (c *Car) GetName() string {
-	return c.Name
-}
-
-func (c *Car) GetPrice() float64 {
-	return c.Price
-}
-
-func (c *Car) SetPrice(price float64) {
-	c.Price = price
-}
-
-func (c *Car) Discount(discount float64) {
-	c.Price -= c.Price * discount / 100
+	ApplyDiscount(discount float64)
 }
 
 func Calculate(products []Product) float64 {
@@ -62,13 +18,13 @@ func Calculate(products []Product) float64 {
 }
 
 func Lab7() {
-	laptop := &Laptop{"Делл XPS 13", 40000.00, "Dell"}
-	car := &Car{"Тойота Королла", 260000.00, "Toyota"}
+	laptop := &Laptop{"Делл XPS 13", 40000.00, "Dell", "Серый"}
+	car := &Car{"Тойота Королла", 260000.00, "Toyota", "Красный"}
 
 	products := []Product{laptop, car}
 	fmt.Println("Общая стоимость", Calculate(products))
 
-	laptop.Discount(17)
-	car.Discount(7)
+	laptop.ApplyDiscount(17)
+	car.ApplyDiscount(7)
 	fmt.Println("Общая стоимость товаров после применения скидок", Calculate(products))
 }
---
+++ golang/LAB_7/laptop.go
@@ -0,0 +1,24 @@
+package LAB_7
+
+type Laptop struct {
+	Name  string
+	Price float64
+	Brand string
+	Color string
+}
+
+func (l *Laptop) GetName() string {
+	return l.Name
+}
+
+func (l *Laptop) GetPrice() float64 {
+	return l.Price
+}
+
+func (l *Laptop) SetPrice(price float64) {
+	l.Price = price
+}
+
+func (l *Laptop) ApplyDiscount(discount float64) {
+	l.Price -= l.Price * discount / 100
+}
