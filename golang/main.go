5fe60d98a0e43d79c8bba146eda1390acdd60789
Slicktty <nik.pantukov@gmail.com>
2024-12-15 19:05:05 +0300

    lab8

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	lab4 "isuct.ru/informatics2022/labs/lab4"
+	lab8 "isuct.ru/informatics2022/labs/lab8"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Пантюков Никита Сергеевич")
+
+	lab4.RunLab4()
+	lab8.RunLab8()
 }
