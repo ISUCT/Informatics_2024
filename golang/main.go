167c6921f611f6bb36ca8cbf3ca8aea0f6e43d9f
Yuno121 <98771348+Yuno121@users.noreply.github.com>
2024-12-15 21:28:52 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	
 	"isuct.ru/informatics2022/lab4"
 	"isuct.ru/informatics2022/lab6"
+	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
 	fmt.Println("TEST")
 	lab4.Lab4AB()
 	lab6.Lab6()
+	lab7.Lab7()
 }
