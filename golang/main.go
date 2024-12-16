b8ea65afff554eeae911cd6fdae23788769a8c99
krezo1206 <krezo1206@gmail.com>
2024-12-17 00:24:23 +0300

    lab6

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
+
+)
 
 func main() {
 	fmt.Println("Ворошилов Ефим Александрович")
+	lab4.RunLab4()
+	lab6.RunLab6()
 }
