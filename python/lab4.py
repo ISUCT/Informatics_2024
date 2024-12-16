339d253bf3997006e9404dd9ff319c622e1e4910
alisa_chin <chindilovaei@gmail.com>
2024-12-16 22:10:27 +0300

    Chindilova_dop_labs

---
+++ python/lab4.py
@@ -0,0 +1,22 @@
+import math 
+
+def CalculateFunction(x, b):
+    y = (1 + (math.sin(b**3 + x**3))**2) / ((b**3 + x**3)**(1/3))
+    return y
+
+def TaskA(xa_beginning, xa_end, xa_delta, b):
+    y_a = []
+    while xa_beginning != (xa_end + xa_delta):
+        y_a.append(CalculateFunction(xa_beginning, b))
+        xa_beginning += xa_delta
+    return y_a
+
+def TaskB(xs, b):
+    y_b = []
+    for i in xs:
+        y_b.append(CalculateFunction(i, b))
+    return y_b
+
+print("Задача А ",TaskA(1.28, 3.28, 0.4, 2.5))
+xs = [1.1, 2.4, 3.6, 1.7, 3.9]
+print("Задача В ",TaskB(xs, 2.5))
---
+++ python/lab6.py
@@ -0,0 +1,29 @@
+class plane:
+    Name : str
+    Flight_number : int
+    Power : int
+    Purchased_seats : int
+
+    def __init__(self, name: str, flight_number : int, power : int, purchased_seats : int):
+        self.Name = name
+        self.Flight_number = flight_number
+        self.Power = power
+        self.Purchased_seats = purchased_seats
+
+    def speed_calculation(self, f_air : int):
+        speed = self.Power / f_air
+        return speed
+    
+    def revenue_calculation(self, price_a_place : int):
+        money = self.Purchased_seats * price_a_place
+        return money
+    
+    def get_routes(self):
+        routes = {1: "Paris-Moscow", 2: "Moscow-Paris", 3: "Moscow-Seoul", 4: "Seoul-Moscow"}
+        nomer = routes.get(self.Flight_number)
+        return nomer
+
+Plane = plane("Airobus999", 3, 200000, 150)
+print("Скорось",Plane.Name , "равна", Plane.speed_calculation(5000) ,"м/с")
+print("Прибыль с продажи белетов составляет:",Plane.revenue_calculation(23000), "рублей")
+print( Plane.Name,"летит по маршруту", Plane.get_routes())
