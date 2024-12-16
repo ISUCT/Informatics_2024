f772a85f0dcf603efb050f77ac20171d7215cefd
Kiruhabri3 <druzininkirill15@gmail.com>
2024-12-16 22:59:30 +0300

    laba8

--- golang/laba4/laba4.go
+++ golang/laba4/laba4.go
@@ -5,31 +5,34 @@ import (
 	"math"
 )
 
-// Функция для вычисления значения
 func calculate(x float64) float64 {
 	return math.Pow(1.2, x) - math.Pow(x, 1.2)
 }
 
-// Функция по задаче 6
-func Y_A() {
-	var begin_x float64 = 0.2
-	var end_x float64 = 2.2
-	var delta_x float64 = 0.4
-
+func Y_A(begin_x, end_x, delta_x float64) {
 	for begin_x < end_x {
 		x := begin_x
-		answer := calculate(x) // Используем новую функцию
+		answer := calculate(x)
 		fmt.Println(answer, x)
 
 		begin_x += delta_x
 	}
 }
 
-// Задача 6.B
-func Y_B() {
-	var arr = [5]float64{0.1, 0.9, 1.2, 1.5, 2.3}
+func Y_B(arr []float64) {
+
 	for _, x := range arr {
-		answer := calculate(x) // Используем новую функцию
+		answer := calculate(x)
 		fmt.Println(answer)
 	}
+}
+
+func RunLaba4() {
+	var begin_x float64 = 0.2
+	var end_x float64 = 2.2
+	var delta_x float64 = 0.4
+	var arr = []float64{0.1, 0.9, 1.2, 1.5, 2.3}
+
+	Y_A(begin_x, end_x, delta_x)
+	Y_B(arr)
 }
--- golang/main.go
+++ golang/main.go
@@ -6,11 +6,13 @@ import (
 	lab6 "isuct.ru/informatics2022/lab6"
 	laba4 "isuct.ru/informatics2022/laba4"
 	laba7 "isuct.ru/informatics2022/laba7"
+	laba8 "isuct.ru/informatics2022/laba8"
 )
 
 func main() {
 	fmt.Println("Дружинин Кирилл Александрович")
-	laba4.Y_B()
+	laba4.RunLaba4()
 	lab6.RunLab6Task()
 	laba7.RunLab7Task()
+	laba8.RunLab8Task()
 }
