cab799a2c8d4eb800aa116251bde7ec68bad0946
Alexander56330 <gog-aleksander.gogolev@yandex.ru>
2024-12-17 20:59:42 +0300

    Update lab8.go

--- golang/lab8/lab8.go
+++ golang/lab8/lab8.go
@@ -1,1 +1,26 @@
+package lab8
 
+import (
+	"fmt"
+	"log"
+)
+
+func Completelab8() {
+	path := "lab8/lab8.txt"
+
+	errCreateFile := CreateFile(path)
+	if errCreateFile != nil {
+		log.Println(errCreateFile)
+	}
+
+	err := WriteFile(path)
+	if err != nil {
+		log.Fatalf("(Completelab8) запись файла: %v", err)
+	}
+
+	searchText, errInput := InputText("текст для поиска")
+	if errInput != nil {
+		log.Fatalf("(Completelab8) ввод текста для поиска: %v", errInput)
+	}
+	fmt.Print(SearchInFile(path, searchText))
+}
