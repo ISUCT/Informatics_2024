b7b8983add8b22d84943594757cf7152d9ab0f7b
Alexander56330 <gog-aleksander.gogolev@yandex.ru>
2024-12-17 20:48:01 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 
 	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
+	"isuct.ru/informatics2022/lab7"
+	"isuct.ru/informatics2022/lab8"
 )
 
 func main() {
 	fmt.Println("Гоголев Александр Сергеевич")
+
+	fmt.Println("5 минут полёт нормальный")
+	
 	lab4.RunLab4()
+	lab6.Completelab6()
+	lab7.Completelab7()
+	lab8.Completelab8()
 }
