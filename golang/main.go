4ae9005689bfb865d27f2736ba62c5ca98580dc0
Skane122 <HARDYWITHUS@yandex.ru>
2024-12-17 18:21:29 +0300

    .

--- golang/main.go
+++ golang/main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	// "isuct.ru/informatics2022/internal/labs/lab4"
-	// "isuct.ru/informatics2022/internal/labs/lab6"
-	// "isuct.ru/informatics2022/internal/labs/lab7"
+	"fmt"
+
+	"isuct.ru/informatics2022/internal/labs/lab4"
+	"isuct.ru/informatics2022/internal/labs/lab6"
+	"isuct.ru/informatics2022/internal/labs/lab7"
 	"isuct.ru/informatics2022/internal/labs/lab8"
-	// "fmt"
 )
 
 func main() {
-	// fmt.Println("Semenenko Mikhail")
-	// resultA := lab4.Task_A(0.22, 0.92, 0.14)
-	// fmt.Println(resultA)
-	// resultB := lab4.Task_B(resultA)
-	// fmt.Println(resultB)
+	fmt.Println("Semenenko Mikhail")
+	resultA := lab4.Task_A(0.22, 0.92, 0.14)
+	fmt.Println(resultA)
+	resultB := lab4.Task_B(resultA)
+	fmt.Println(resultB)
 
-	// lab6.RunLab6()
+	lab6.RunLab6()
 
-	// lab7.RunLab7()
+	lab7.RunLab7()
 
 	lab8.RunLab8()
 }
