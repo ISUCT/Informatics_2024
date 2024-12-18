53e79959485889d40dcb2b504574c97d8f162320
dusyaT <titovadasa21@gmail.com>
2024-12-18 20:02:01 +0300

    Update lab7.go

--- golang/labs/lab7/lab7.go
+++ golang/labs/lab7/lab7.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func Start7lab() {
 	product1 := &Clothes{Name: "Шорты", Price: 1000, Size: "S", Color: "White"}
-	product1.make_sale(500)
+	product1.make_sale(100)
 	product2 := &Vegetables{Name: "Перец", Weight: 100, Price: 100}
 	product2.make_sale(50)
 	product3 := &Book{Name: "Алиса в стране чудес", Format: "бумажный", Price: 450}
