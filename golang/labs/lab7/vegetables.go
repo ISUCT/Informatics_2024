ab3c0c1dc8bef23f455379d45b7093f56290e518
dusyaT <titovadasa21@gmail.com>
2024-12-18 19:08:28 +0300

    Update vegetables.go

--- golang/labs/lab7/vegetables.go
+++ golang/labs/lab7/vegetables.go
@@ -11,9 +11,11 @@ type Vegetables struct {
 func (c *Vegetables) make_sale(discount float64) {
 	c.Price = c.Price * (1 - discount/100)
 }
+
 func (c *Vegetables) get_price() float64 {
 	return c.Price
 }
+
 func (c *Vegetables) get_productInfo() string {
-	return fmt.Sprintf("Name: %s, Weight %s, Price: %.2f", c.Name, c.Weight, c.Price)
+    return fmt.Sprintf("Name: %s, Weight: %.2f, Price: %.2f", c.Name, c.Weight, c.Price)
 }
