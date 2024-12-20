5fc343c4b3de1f392fe9f9c0e3f276e4d5df13b4
Shumskaya Daria <110334152+dipmej@users.noreply.github.com>
2024-12-21 00:11:37 +0300

    Update lab4.go

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math"
 )
-
 func Calculate(a, b, x float64) float64 {
 	var y float64 = (1 + math.Pow(math.Log10(x/a), 2)) / (b - math.Pow(math.Exp(1), (x/a)))
 	return y
@@ -16,7 +15,6 @@ func CompleteTaskA(a, b, xbegin, xend, xdelt float64) []float64 {
 	}
 	return result
 }
-
 func CompleteTaskB(a, b float64, xs []float64) []float64 {
 	var result []float64
 	for _, x := range xs {
@@ -33,12 +31,10 @@ func Answerlab() {
 	var xs []float64 = []float64{2.2, 3.78, 4.51, 6.58, 1.2}
 	var for_a = CompleteTaskA(a, b, xbegin, xend, xdelt)
 	var for_b = CompleteTaskB(a, b, xs)
-
 	fmt.Println("Решение задания А:")
 	for i, y := range for_a {
 		fmt.Printf("y%d = %f\n", i+1, y)
 	}
-
 	fmt.Println("Решение задания B:")
 	for i, y := range for_b {
 		fmt.Printf("y%d = %f\n", i+1, y)
