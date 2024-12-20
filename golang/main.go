570a3a11264289b65f8a41b663f0ae1f2a222851
Иван Яшков <ivanaskov@MacBook-Pro-Ivan.local>
2024-12-20 16:23:46 +0300

    lab 4 and 6

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	lab4 "isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Yashkov Ivan")
+	lab4.RunLab4()
+	lab6.RunLab6()
 }
