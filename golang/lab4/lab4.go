1625e7a1d0b119721f1bf94b0e2788e7728e5aa9
Shumskaya Daria <110334152+dipmej@users.noreply.github.com>
2024-12-20 22:12:55 +0300

    Update lab4.go

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,6 +1,7 @@
 package lab4
 
-import "math"
+import ("fmt"
+	"math")
 
 func Calculate(a, b, x float64) float64 {
 	var y float64 = (1 + math.Pow(math.Log10(x/a), 2)) / (b - math.Pow(math.Exp(1), (x/a)))
@@ -15,10 +16,31 @@ func CompleteTaskA(a, b, xbegin, xend, xdelt float64) []float64 {
 	return result
 }
 
-func CompleteTaskB(a, b float64, x []float64) []float64 {
+func CompleteTaskB(a, b float64, xs []float64) []float64 {
 	var result []float64
-	for i := 0; i < len(x); i++ {
-		result = append(result, Calculate(a, b, x[i]))
+	for _, x := range xs {
+		result = append(result, Calculate(a, b, x))
 	}
 	return result
+}
+func Answerlab() {
+	var a float64 = 2.0
+	var b float64 = 0.95
+	var xbegin float64 = 1.25
+	var xend float64 = 2.75
+	var xdelt float64 = 0.3
+	var xs []float64 = []float64{2.2, 3.78, 4.51, 6.58, 1.2}
+	var for_a = CompleteTaskA(a, b, xbegin, xend, xdelt)
+	var for_b = CompleteTaskB(a, b, xs)
+
+	fmt.Println("Решение задания А:")
+	for i, y := range for_a {
+		fmt.Printf("y%d = %f\n", i+1, y)
+	}
+
+	fmt.Println("Решение задания B:")
+	for i, y := range for_b {
+		fmt.Printf("y%d = %f\n", i+1, y)
+	}
+
 }
