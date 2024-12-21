f3d9c817db1406208d30a1f0311038106b711734
pulbeem <anastasiakolp22@icloud.com>
2024-12-21 15:12:46 +0300

    Update book.go

--- golang/lab7/book.go
+++ golang/lab7/book.go
@@ -3,9 +3,9 @@ package lab7
 import "fmt"
 
 type Book struct {
-	Name  string
+	Name   string
 	Format string
-	Price float64
+	Price  float64
 }
 
 func (b *Book) GetInfo() {
@@ -16,18 +16,18 @@ func (b *Book) GetPrice() float64 {
 	return b.Price
 }
 
-func (b *Book) MakeDiscount(x float64) {
-	if x >= 100 {
+func (b *Book) MakeDiscount(discountPercentage float64) {
+	if discountPercentage >= 100 {
 		b.Price = 0
 		return
 	}
-	b.Price = b.Price * (1 - x/100)
+	b.Price = b.Price * (1 - discountPercentage/100)
 }
 
-func (b *Book) ChangePrice(x float64) {
-	b.Price = x
+func (b *Book) ChangePrice(newPrice float64) {
+	b.Price = newPrice
 }
 
-func (b *Book) ChangeDescription(x string) {
-	b.Format = x
+func (b *Book) ChangeDescription(newFormat string) {
+	b.Format = newFormat
 }
