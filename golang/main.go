d1c442f9dd5863568f6c4896e5e520dbf1edc6c0
User of DK310 <dk310@it.isuct.ru>
2024-12-21 09:26:40 +0300

    вернул недостающее в main

--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
+	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
 	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
+	fmt.Println("Щербаков Илья Алексадрович")
+	fmt.Println("Task А:\n", lab4.TaskA(0.95, 2.0, 1.25, 2.75, 0.3))
+	fmt.Println("Task В:\n", lab4.TaskB(2.0, 0.95, [5]float64{2.2, 3.78, 4.51, 6.58, 1.2}))
+	lab6.RunLab6()
 	lab7.RunLab7()
 }
