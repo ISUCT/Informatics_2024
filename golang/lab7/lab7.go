b7fb2d133f94bda64764d1413045f5fd490657bd
Yuno121 <98771348+Yuno121@users.noreply.github.com>
2024-12-21 09:53:32 +0300

    Update lab7.go

--- golang/lab7/lab7.go
+++ golang/lab7/lab7.go
@@ -2,63 +2,36 @@ package lab7
 
 import "fmt"
 
-// Интерфейс Product
 type Product interface {
 	GetPrice() float64
 	SetPrice(price float64)
-	GetDiscount() float64
-	SetDiscount(discount float64)
+	ApplyDiscount(discount float64)
 	GetName() string
 	SetName(name string)
 }
 
-// Структура ProductBase
-type ProductBase struct {
-	price    float64
-	discount float64
-	name     string
-}
-
-func (p *ProductBase) GetPrice() float64 {
-	return p.price
-}
-
-func (p *ProductBase) SetPrice(price float64) {
-	p.price = price
-}
-
-func (p *ProductBase) GetDiscount() float64 {
-	return p.discount
-}
-
-func (p *ProductBase) SetDiscount(discount float64) {
-	p.discount = discount
-}
-
-func (p *ProductBase) GetName() string {
-	return p.name
-}
-
-func (p *ProductBase) SetName(name string) {
-	p.name = name
-}
-
-// Функция для расчета стоимости списка товаров
 func CalculateTotalPrice(products []Product) float64 {
 	totalPrice := 0.0
 	for _, product := range products {
-		totalPrice += product.GetPrice() * (1 - product.GetDiscount())
+		totalPrice += product.GetPrice()
 	}
 	return totalPrice
 }
 
 func Lab7() {
-	// Создание продуктов
-	iphone := &ProductBase{price: 100, discount: 0.1, name: "Айфон"}
-	ipad := &ProductBase{price: 200, discount: 0.2, name: "Айпад"}
+	iphone := &Phone{price: 100, weight: 200, name: "Айфон"}
+	tshirt := &Shirt{price: 200, size: "S", name: "Футболка"}
+	apple := &Fruits{price: 50, calories: 52, name: "Яблоко"}
 
-	// Расчет стоимости списка товаров
-	totalPrice := CalculateTotalPrice([]Product{iphone, ipad})
-	fmt.Println("Общая цена:", totalPrice)
+	products := []Product{iphone, tshirt, apple}
+
+	totalPrice := CalculateTotalPrice(products)
+	fmt.Println("Общая цена без скидки:", totalPrice)
+
+	products[0].ApplyDiscount(10)
+	products[1].ApplyDiscount(20)
+	products[2].ApplyDiscount(25)
+
+	totalPrice = CalculateTotalPrice(products)
+	fmt.Println("Общая цена с скидкой:", totalPrice)
 }
-
