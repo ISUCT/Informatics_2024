d131e697fe1cd2f24495a914603a2074655ce0ed
KA1SER-06 <aspinfernus@gmail.com>
2024-12-22 14:25:10 +0300

    Lab4-update

--- golang/main.go
+++ golang/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 
+	Lab4 "isuct.ru/informatics2022/Labs/Lab4"
 	lab6 "isuct.ru/informatics2022/Labs/Lab6"
-	lab4 "isuct.ru/informatics2022/labs/lab4"
 	lab7 "isuct.ru/informatics2022/labs/lab7"
 )
 
 func main() {
 	fmt.Println("Maksimov_Daniil_Andreevich")
 
-	lab4.RunLab4()
+	Lab4.RunLab4()
 	lab6.Lab6()
 	lab7.RunLab7()
 }
