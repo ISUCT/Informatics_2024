b42ac421c3d06af71f7dc7e2cb17d0321d84d197
Dmitriy Belov <dimabelov2006@gmail.com>
2024-12-23 18:11:46 +0300

    fix linter python

--- python/src/main.py
+++ python/src/main.py
@@ -1,6 +1,11 @@
 from src.lab4 import lab4
 from src.lab6 import lab6
 
+
+def summ(a: int, b: int) -> int:
+    return a + b
+
+
 if __name__ == "__main__":
     lab4.RunLab4Tasks()
     lab6.RunLab6Tasks()
