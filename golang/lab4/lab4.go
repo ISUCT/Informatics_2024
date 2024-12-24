7997e3057e6100a6c0689e4b5fd94594f98e3015
krezo1206 <krezo1206@gmail.com>
2024-12-24 13:53:53 +0300

    переделал 4ю лабораторную

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -1,4 +1,4 @@
-package lab4
+package LAB_4
 
 import (
 	"fmt"
@@ -6,29 +6,26 @@ import (
 )
 
 func Calculate(x float64) float64 {
-	return math.Pow(math.Abs(x*x-2.5), 0.25) + math.Pow(math.Log10(x*x), 0.33333333)
+	return math.Pow(math.Abs(x*x-2.5), 0.25) + math.Pow(math.Log10(x*x), 0.33)
 }
-
-func Task_A(begin_x, end_x, delta_x float64) []float64 {
-	var result []float64
-
-	for x := begin_x; x < end_x; x += delta_x {
-		result = append(result, Calculate(x))
+func TaskA(sx, ex, stepx float64) []float64 {
+	var ValueY []float64
+	for x := sx; x < ex; x += stepx {
+		ValueY = append(ValueY, Calculate(x))
 	}
-	return result
+	return ValueY
 }
-
-func Task_B(arguments []float64) []float64 {
-	var result []float64
+func TaskB(arguments []float64) []float64 {
+	var ValueY []float64
 
 	for _, x := range arguments {
-		result = append(result, Calculate(x))
+		ValueY = append(ValueY, Calculate(x))
 	}
-	return result
+	return ValueY
 }
 
 func RunLab4() {
 	x := []float64{1.84, 2.71, 3.81, 4.56, 5.62}
-	fmt.Println("Задача A", Task_A(1.25, 3.25, 0.4))
-	fmt.Println("Задача B", Task_B(x))
+	fmt.Println("задание a", TaskA(1.25, 3.25, 0.4))
+	fmt.Println("задание b", TaskB(x))
 }
