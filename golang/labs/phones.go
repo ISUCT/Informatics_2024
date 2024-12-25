85e1c869174bc897ceaf9c1a00dfee7cefcebdcf
Artyom <real.warface.82@mail.ru>
2024-12-25 23:05:53 +0300

    Update phones.go

--- golang/labs/phones.go
+++ golang/labs/phones.go
@@ -3,36 +3,48 @@ package labs
 import "fmt"
 
 type Phones struct {
-  brand string
-  price float64
-  name  string
-  color string
+	brand string
+	price float64
+	name  string
+	color string
 }
 
 func (p *Phones) getPrice() float64 {
-  return p.price
+	return p.price
 }
 
 func (p *Phones) setPrice(newPrice float64) {
-  p.price = newPrice
+	p.price = newPrice
 }
 
 func (p *Phones) applyDiscount(discount float64) {
-  if discount < 0 || discount > 100 {
-    fmt.Println("Скидка должна быть в диапазоне от 0 до 100%")
-    return
-  }
-  p.price = p.price * (1 - discount/100)
+	if discount < 0 || discount > 100 {
+		fmt.Println("Скидка должна быть в диапазоне от 0 до 100%")
+		return
+	}
+	p.price = p.price * (1 - discount/100)
 }
 
 func (p *Phones) getName() string {
-  return p.name
+	return p.name
 }
 
 func (p *Phones) setName(newName string) {
-  p.name = newName
+	p.name = newName
+}
+
+func (p *Phones) getBrand() string {
+	return p.brand
 }
 
 func (p *Phones) setBrand(newBrand string) {
-  p.brand = newBrand
+	p.brand = newBrand
+}
+
+func (p *Phones) getColor() string {
+	return p.color
+}
+
+func (p *Phones) setColor(newColor string) {
+	p.color = newColor
 }
