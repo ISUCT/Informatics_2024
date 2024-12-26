7b4cbb3d3447e878b8ddf05f4b84b7f3e7e5e8d5
rukinapolina <polinostka@gmail.com>
2024-12-26 22:46:46 +0300

    Update lab8Lab4.go

---
+++ golang/lab8/lab8Lab4.go
@@ -0,0 +1,48 @@
+package lab4 
+ 
+import ( 
+    "math"
+    "fmt"
+	"isuct.ru/informatics2022/lab8"
+	"strconv"
+
+)
+func  CalculateExpression (a float64, x float64) float64 { 
+    return math.Tan(math.Pow(math.Log10(a+x), 3)) / math.Pow(a+x, 2.0/7.0) 
+} 
+ 
+func TaskA(a , xi , xk , deltax float64) []float64 { 
+     var values []float64 
+    for x := xi; x <= xk; x += deltax { 
+        values = append(values,  CalculateExpression (a, x)) 
+    } 
+    return values 
+} 
+ 
+func ReadFileTask4() []float64 {
+	fmt.Println("Вводите данные в следующем порядке: xi , xk , deltax для задачи A и values для задачи B")
+	arr := lab8.RunLab8Tasks()
+	var numbers []float64
+	for _, values := range arr {
+	 value, _ := strconv.ParseFloat(values, 64)
+	 numbers = append(numbers, value)
+	}
+	return numbers
+   }
+   func TaskB(a float64, x [5]float64) []float64 { 
+    var values []float64
+    for _, value := range x {
+        values = append(values,  CalculateExpression (a, value)) 
+    } 
+    return values
+	}
+   
+   func RunLab4Tasks() {
+	const a float64 = 2.0
+	arr := ReadFileTask4()
+	values := arr[3:]
+	ValuesA := TaskA(a, arr[0], arr[1], arr[2])
+	ValuesB := TaskB(a, values)
+	PrintValue(ValuesA)
+	PrintValue(ValuesB)
+   }
