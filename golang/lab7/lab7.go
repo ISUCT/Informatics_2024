3cfda88527c8948a9e82caf580fe8dfaa887d00b
rukinapolina <polinostka@gmail.com>
2024-12-26 22:44:59 +0300

    lab8

--- golang/lab7/lab7.go
+++ golang/lab7/lab7.go
@@ -1,43 +1,40 @@
-package lab7
-
-import "fmt"
-
-type Products interface {
-	GetInformation()
-	GetPrice() float32
-	ApplyDiscount(float32)
-	ChangePrice(float32)
-	ChangeDescription(string)
-}
-
-
-func CalculateBuy(list []Products) float32 {
-	var sum float32 = 0
-	for _, price := range list {
-		sum += price.GetPrice()
-	}
-	return sum
-}
-
-func RunLab7Tasks() {
-	var cosmetic Products = &Cosmetic{"помада", "Gucci", 5000}
-	var techcic Products = &Technic{"ноутбук", "Apple", 169660}
-	var sweets Products = &Sweets{"Сникерс", "Солёная карамель", 85}
-	cosmetic.GetInformation()
-	techcic.GetInformation()
-	sweets.GetInformation()
-	var buy []Products = []Products{cosmetic , techcic, sweets}
-	sum := CalculateBuy(buy)
-	fmt.Println("Товары стоят",sum)
-	techcic.ChangePrice(150000)
-	cosmetic.ChangeDescription("LV")
-	cosmetic.ApplyDiscount(5)
-	sweets.ApplyDiscount(60)
-	sweets.ChangeDescription("Белый шоколад")
-	techcic.GetInformation()
-	cosmetic.GetInformation()
-	sweets.GetInformation()
-
-	sum = CalculateBuy(buy)
-	fmt.Println("Товары стоят",sum)
+package lab7 
+ 
+import "fmt" 
+ 
+type Products interface { 
+ GetInformation() 
+ GetPrice() float32 
+ ApplyDiscount(float32) 
+ ChangePrice(float32) 
+ ChangeDescription(string) 
+} 
+ 
+ 
+func CalculateBuy(list []Products) float32 { 
+ var sum float32 = 0 
+ for _, price := range list { 
+  sum += price.GetPrice() 
+ } 
+ return sum 
+} 
+ 
+func RunLab7Tasks() { 
+ var cosmetic Products = &Cosmetic{"помада", "Gucci", 5000} 
+ var techcic Products = &Technic{"ноутбук", "Apple", 169660} 
+ var sweets Products = &Sweets{"Сникерс", "Солёная карамель", 85} 
+ cosmetic.GetInformation() 
+ techcic.GetInformation() 
+ sweets.GetInformation() 
+ var buy []Products = []Products{cosmetic , techcic, sweets} 
+ sum := CalculateBuy(buy) 
+ fmt.Println("Товары стоят",sum) 
+ techcic.ChangePrice(150000) 
+ cosmetic.ChangeDescription("LV") 
+ cosmetic.ApplyDiscount(5) 
+ sweets.ApplyDiscount(60) 
+ sweets.ChangeDescription("Белый шоколад") 
+ techcic.GetInformation() 
+ cosmetic.GetInformation() 
+ sweets.GetInformation() 
 }
---
+++ golang/lab8/lab8.go
@@ -0,0 +1,112 @@
+package lab8
+import(
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strings"
+	"io"
+)
+func CreateNewFile (name string){
+	file, err:= os.Create(name)
+	if err != nill{
+		panic(err)
+	} 
+	defer file.Close()
+}
+func InputData() (float64, []float64) {
+	var a float64
+	fmt.Print("Введите значение a: ")
+	fmt.Scan(&a)
+   
+	var n int
+	fmt.Print("Введите количество значений x: ")
+	fmt.Scan(&n)
+   
+	xValues := make([]float64, n)
+	for i := 0; i < n; i++ {
+	 fmt.Printf("Введите значение x[%d]: ", i+1)
+	 fmt.Scan(&xValues[i])
+	}
+   
+	return a, xValues
+   }
+
+func WriteValuesToFile(filename string, a float64, xValues []float64) error {
+	file, err := os.Create(filename)
+	if err != nil {
+	 return err
+	}
+	defer file.Close()
+   
+	_, err = file.WriteString(fmt.Sprintf("%f\n", a))
+	if err != nil {
+	 return err
+	}
+   
+	for _, x := range xValues {
+	 _, err = file.WriteString(fmt.Sprintf("%f\n", x))
+	 if err != nil {
+	  return err
+	 }
+	}
+	return nil
+   }
+
+func ReadInputFromFile(filename string)(float64, []float64,error){
+	file, err := os.Open(filename)
+	if err != hill{
+		return 0, nill, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+ var a float64
+ var xValues []float64
+
+ if scanner.Scan() {
+  a, err = strconv.ParseFloat(scanner.Text(), 64)
+  if err != nil {
+   return 0, nil, err
+  }
+ }
+}
+for scanner.Scan() {
+	x, err := strconv.ParseFloat(scanner.Text(), 64)
+	if err != nil {
+	 return 0, nil, err
+	}
+	xValues = append(xValues, x)
+   }
+  
+   return a, xValues, scanner.Err()
+  func PrintFileContent(filename string) error {
+   file, err := os.Open(filename)
+   if err != nil {
+	return err
+   }
+   defer file.Close()
+  
+   scanner := bufio.NewScanner(file)
+   for scanner.Scan() {
+	fmt.Println(scanner.Text())
+   }
+   return scanner.Err()
+  }
+  func SearchInFile(filename string, searchString string) ([]string, error) {
+   file, err := os.Open(filename)
+   if err != nil {
+	return nil, err
+   }
+   defer file.Close()
+  
+   var results []string
+   scanner := bufio.NewScanner(file)
+   for scanner.Scan() {
+	line := scanner.Text()
+	if contains(line, searchString) {
+	 results = append(results, line)
+	}
+   }
+   return results, scanner.Err()
+  }
+  
