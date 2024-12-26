7092f3c6abe18d2b90174785d5bca252fc4723fb
Viktoria3284 <vikamedvedeva879@gmail.com>
2024-12-26 16:18:42 +0300

    исправила 4 лабораторную

--- golang/labs/lab4/lab4.go
+++ golang/labs/lab4/lab4.go
@@ -1,3 +1,5 @@
+package lab4
+
 import (
 	"fmt"
 	"math"
@@ -6,12 +8,12 @@ import (
 func RunLab4() {
 	a := 2.5
 	b := 4.6
-	fmt.Println(TaskA(1.1, 3.6, 0.5, a, b))
+	fmt.Println(taska(1.1, 3.6, 0.5, a, b))
 	arr := []float64{1.2, 1.28, 1.36, 1.46, 2.35}
-	fmt.Println(TaskB(arr, a, b))
+	fmt.Println(taskb(arr, a, b))
 }
 
-func TaskA(xn, xk, deltax, a, b float64) []float64 {
+func taska(xn, xk, deltax, a, b float64) []float64 {
 	var yValues []float64
 	for x := xn; x <= xk; x += deltax {
 		yValues = append(yValues, CalculateY(x, a, b))
@@ -19,7 +21,7 @@ func TaskA(xn, xk, deltax, a, b float64) []float64 {
 	return yValues
 }
 
-func TaskB(values []float64, a, b float64) []float64 {
+func taskb(values []float64, a, b float64) []float64 {
 	var yValues []float64
 	for _, x := range values {
 		yValues = append(yValues, CalculateY(x, a, b))
