4b8977bf3128e28b73af5414511667a46463da76
Milanaaa0 <milanchik100@mail.ru>
2024-12-27 13:55:35 +0300

    fixed

--- golang/main.go
+++ golang/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	lab4 "isuct.ru/informatics2022/labs/lab4"
-	lab6 "isuct.ru/informatics2022/labs/lab6"
-	lab7 "isuct.ru/informatics2022/labs/lab7"
-	lab8 "isuct.ru/informatics2022/labs/lab8"
-	lab9 "isuct.ru/informatics2022/labs/lab9"
+	"isuct.ru/informatics2022/labs/lab4"
+	"isuct.ru/informatics2022/labs/lab6"
+	"isuct.ru/informatics2022/labs/lab7"
+	"isuct.ru/informatics2022/labs/lab8"
+	"isuct.ru/informatics2022/labs/lab9"
 )
 
 func main() {
