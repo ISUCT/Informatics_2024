4a38b9c0cdb28a85769cdcfa5d7e5b01fca2a7b4
Skane122 <HARDYWITHUS@yandex.ru>
2024-12-27 18:49:32 +0300

    Fixed Lab8

--- golang/internal/labs/lab8/lab8forlab4.go
+++ golang/internal/labs/lab8/lab8forlab4.go
@@ -3,9 +3,10 @@ package lab8
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
+
+	"isuct.ru/informatics2022/internal/labs/lab4"
 )
 
 func ReadData(filename string) ([]float64, error) {
@@ -27,28 +28,6 @@ func ReadData(filename string) ([]float64, error) {
 	return numbers, nil
 }
 
-func Calculate(x float64) float64 {
-	var n float64 = 1.0 / 7
-	return math.Pow(math.Pow(math.Asin(x), 4)+math.Pow(math.Acos(x), 6), n)
-}
-
-func Task_A(beginx float64, endx float64, stepx float64) []float64 {
-	answers := []float64{}
-	for x := beginx; x <= endx; x += stepx {
-		result := Calculate(x)
-		answers = append(answers, result)
-	}
-	return answers
-}
-
-func Task_B(arr []float64) []float64 {
-	answers := []float64{}
-	for _, x := range arr {
-		answers = append(answers, Calculate(x))
-	}
-	return answers
-}
-
 func RunLab8forLab4(filename string) error {
 	values, err := ReadData(filename)
 	if err != nil {
@@ -59,11 +38,11 @@ func RunLab8forLab4(filename string) error {
 	var stepx float64 = values[2]
 	var taskBslice = values[3:]
 	fmt.Println("Задача А")
-	for i, value := range Task_A(beginx, endx, stepx) {
+	for i, value := range lab4.Task_A(beginx, endx, stepx) {
 		fmt.Printf("x = %d\ty = %f\n", i, value)
 	}
 	fmt.Println("Задание B")
-	for i, value := range Task_B(taskBslice) {
+	for i, value := range lab4.Task_B(taskBslice) {
 		fmt.Printf("x = %d\ty = %f\n", i, value)
 	}
 	return nil
