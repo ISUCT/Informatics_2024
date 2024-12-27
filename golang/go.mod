4093a3af149fc63d8d3bbb8bfd23fded82052bea
Voronkov_Klim <Klimysha@exmple.com>
2024-12-28 01:18:35 +0300

    Переделал как надо

--- golang/go.mod
+++ golang/go.mod
@@ -2,4 +2,7 @@ module isuct.ru/informatics2022
 
 go 1.16
 
-require github.com/stretchr/testify v1.8.1
+require (
+	github.com/stretchr/testify v1.8.1
+	rsc.io/quote v1.5.2 // indirect
+)
--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/LABS/Lab4/lab4"
+
+	lab6 "isuct.ru/informatics2022/LABS/Lab6/Lab6"
+)
 
 func main() {
+
 	fmt.Println("Voronkov Klim Sergeevich")
+	lab6.Runlab6()
+	lab4.RunLab4()
 }
