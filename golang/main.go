841cb2c5baf330561a6a5f41f77cf8a93cae489b
Shumskaya Daria <110334152+dipmej@users.noreply.github.com>
2024-12-28 01:09:40 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"isuct.ru/informatics2022/labs_group/lab4"
 	"isuct.ru/informatics2022/labs_group/labs/lab6"
-	"gisuct.ru/informatics2022/labs_group/labs/lab7"
+	"isuct.ru/informatics2022/labs_group/labs/lab7"
 )
 
 func main() {
