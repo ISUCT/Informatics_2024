193a57acac0f4911c3a603a51649550b29c7c11a
xsssvake <svakewow@gmail.com>
2024-12-28 14:26:56 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
+	"isuct.ru/informatics2022/lab4"
+
 	"isuct.ru/informatics2022/lab6"
 )
 
 func main() {
 	fmt.Println("Нерабеев Кирилл Сергеевич")
+	lab4.Lab4()
 	lab6.Lab6()
 }
