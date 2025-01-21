058d692b82e0fccb9bd3b7e185fba81d85a2f683
Agurchic <alex-kekihc@list.ru>
2025-01-22 00:11:43 +0300

    linter

--- golang/lab/lab4/lab4.go
+++ golang/lab/lab4/lab4.go
@@ -1,54 +1,25 @@
 package lab4_test
 
 import (
-	"fmt"
-	"math"
+ "math"
 )
 
 func CalculateY(a, b, x float64) float64 {
-	return math.Pow((a*x+b), 1.0/3.0) / math.Pow(math.Log10(x), 2)
+ return math.Pow((a*x+b), 1.0/3.0) / math.Pow(math.Log10(x), 2)
 }
 
 func TaskA(a, b, xStart, xEnd, step float64) []float64 {
-	var result []float64
-	for x := xStart; x <= xEnd; x += step {
-		result = append(result, CalculateY(a, b, x))
-	}
-	return result
+ var result []float64
+ for x := xStart; x <= xEnd; x += step {
+  result = append(result, CalculateY(a, b, x))
+ }
+ return result
 }
 
 func TaskB(a, b float64, xValues []float64) []float64 {
-	var result []float64
-	for _, x := range xValues {
-		result = append(result, CalculateY(a, b, x))
-	}
-	return result
-}
-
-func labs() {
-	var a, b, xStart, xEnd, step float64
-
-	a = 0.1
-	b = 0.5
-	xStart = 0.33
-	xEnd = 1.23
-	step = 0.18
-	var xValues []float64 = []float64{0.5, 0.36, 0.40, 0.62, 0.78}
-	var resultA []float64 = TaskA(a, b, xStart, xEnd, step)
-	var resultB []float64 = TaskB(a, b, xValues)
-
-	fmt.Println("Результаты задачи A:")
-	for i, y := range resultA {
-		x := xStart + step*float64(i)
-		fmt.Printf("x = %.2f, y = %.4f\n", x, y)
-	}
-
-	fmt.Println("\nРезультаты задачи B:")
-	for i, y := range resultB {
-		fmt.Printf("x = %.2f, y = %.4f\n", xValues[i], y)
-	}
-}
-
-func lab4() {
-	labs()
+ var result []float64
+ for _, x := range xValues {
+  result = append(result, CalculateY(a, b, x))
+ }
+ return result
 }
--- golang/lab/lab6/lab6.go
+++ golang/lab/lab6/lab6.go
@@ -3,27 +3,20 @@ package tests
 import "fmt"
 
 type Dog struct {
-	Name  string
-	Breed string
-	Age   int
+    Name  string
+    Breed string
+    Age   int
 }
 
 func NewDog(name string, breed string, age int) Dog {
-	return Dog{Name: name, Breed: breed, Age: age}
+    return Dog{Name: name, Breed: breed, Age: age}
 }
 func (d Dog) GetAge() int {
-	return d.Age
+    return d.Age
 }
 func (d *Dog) SetAge(age int) {
-	d.Age = age
+    d.Age = age
 }
 func (d Dog) GetInfo() string {
-	return fmt.Sprintf("Name: %s, Breed: %s, Age: %d", d.Name, d.Breed, d.Age)
-}
-func inf() {
-	myDog := NewDog("Buddy", "Labrador", 3)
-	fmt.Println(myDog.GetInfo())
-	fmt.Println("Age:", myDog.GetAge())
-	myDog.SetAge(4)
-	fmt.Println(myDog.GetInfo())
+    return fmt.Sprintf("Name: %s, Breed: %s, Age: %d", d.Name, d.Breed, d.Age)
 }
